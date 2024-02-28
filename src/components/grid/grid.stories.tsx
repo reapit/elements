c29da240d5bec4b663759cdd108431ab49cc76af
Ashleigh Simonelli <ashleighsimonelli@gmail.com>
2024-02-28 08:48:16 +0000

    refactor: updated argstable to controls for v8

--- src/components/grid/grid.stories.tsx
+++ src/components/grid/grid.stories.tsx
@@ -4,6 +4,7 @@ import { GridResponsive, ColResponsive } from './grid-responsive'
 import { MediaStateProvider } from '../../hooks/use-media-query'
 import { cx } from '@linaria/core'
 import { elColGap2, elRowGap3, elSpan12, elSpan8, elSpan4, elOffset4, elOffset8 } from './__styles__/units'
+import { StoryObj } from '@storybook/react'
 
 export default {
   title: 'Grid',
