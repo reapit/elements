f4f0fc4cf4a1d24b64391a1f3bc84d13311dcfb7
Ashleigh Simonelli <ashleighsimonelli@gmail.com>
2024-02-28 16:02:21 +0000

    feat: tooltip aria labels

--- src/components/pagination/pagination.tsx
+++ src/components/pagination/pagination.tsx
@@ -100,11 +100,21 @@ export const Pagination: FC<PaginationProps> = ({
   const prevPage = currentPage > 1 ? currentPage - 1 : null
   return (
     <PaginationWrap {...rest}>
-      <PaginationButton role="button" aria-label="Previous" data-testid="back-button" onClick={handlePageChange(prevPage, callback, setInputValue)}>
+      <PaginationButton
+        role="button"
+        aria-label="Previous"
+        data-testid="back-button"
+        onClick={handlePageChange(prevPage, callback, setInputValue)}
+      >
         <Icon icon="chevronLeft" className={cx(prevPage && elPaginationPrimary)} />
       </PaginationButton>
       {hasStartButton && (
-        <PaginationButton role="button" aria-label="Previous" data-testid="back-button" onClick={handlePageChange(1, callback, setInputValue)}>
+        <PaginationButton
+          role="button"
+          aria-label="Previous"
+          data-testid="back-button"
+          onClick={handlePageChange(1, callback, setInputValue)}
+        >
           <Icon icon="chevronLeft" className={cx(currentPage !== 1 && elPaginationPrimary)} />
           <Icon icon="chevronLeft" className={cx(currentPage !== 1 && elPaginationPrimary)} />
         </PaginationButton>
@@ -113,12 +123,22 @@ export const Pagination: FC<PaginationProps> = ({
         <PaginationInput onChange={handlePageInput(setInputValue)} value={inputValue} /> of {numberPages}
       </PaginationText>
       {hasEndButton && (
-        <PaginationButton role="button" aria-label="Next" data-testid="back-button" onClick={handlePageChange(numberPages, callback, setInputValue)}>
+        <PaginationButton
+          role="button"
+          aria-label="Next"
+          data-testid="back-button"
+          onClick={handlePageChange(numberPages, callback, setInputValue)}
+        >
           <Icon icon="chevronRight" className={cx(currentPage !== numberPages && elPaginationPrimary)} />
           <Icon icon="chevronRight" className={cx(currentPage !== numberPages && elPaginationPrimary)} />
         </PaginationButton>
       )}
-      <PaginationButton role="button" aria-label="Next" data-testid="forward-button" onClick={handlePageChange(nextPage, callback, setInputValue)}>
+      <PaginationButton
+        role="button"
+        aria-label="Next"
+        data-testid="forward-button"
+        onClick={handlePageChange(nextPage, callback, setInputValue)}
+      >
         <Icon icon="chevronRight" className={cx(nextPage && elPaginationPrimary)} />
       </PaginationButton>
     </PaginationWrap>
