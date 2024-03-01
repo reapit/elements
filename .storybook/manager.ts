97e86504b9dde59ef6a54993487f758bdb9cd333
Ashleigh Simonelli <ashleighsimonelli@gmail.com>
2024-03-01 10:42:17 +0000

    fix: corrected theming

--- .storybook/manager.ts
+++ .storybook/manager.ts
@@ -1,41 +1,37 @@
-// import { addons } from '@storybook/addon-essentials'
 import { create } from '@storybook/theming'
 import { version } from '../package.json'
-// import logo from './public/reapitLogo.svg'
 
-// addons.setConfig({
-//   theme: create({
-//     base: 'light',
+create({
+  base: 'light',
 
-//     colorPrimary: '#4e56ea',
-//     colorSecondary: '#4e56ea',
+  colorPrimary: '#4e56ea',
+  colorSecondary: '#4e56ea',
 
-//     // UI
-//     appBg: '#fff',
-//     appContentBg: '#fff',
-//     appBorderColor: '#4e56ea',
-//     appBorderRadius: 0,
+  // UI
+  appBg: '#fff',
+  appContentBg: '#fff',
+  appBorderColor: '#4e56ea',
+  appBorderRadius: 0,
 
-//     // Typography
-//     fontBase: "'Inter', sans-serif",
-//     fontCode: "'Source Code Pro', monospace",
+  // Typography
+  fontBase: "'Inter', sans-serif",
+  fontCode: "'Source Code Pro', monospace",
 
-//     // Text colors
-//     textColor: '#222b33',
-//     textInverseColor: '#fff',
+  // Text colors
+  textColor: '#222b33',
+  textInverseColor: '#fff',
 
-//     // Toolbar default and active colors
-//     barTextColor: '#222b33',
-//     barSelectedColor: '#4e56ea',
-//     barBg: '#fff',
+  // Toolbar default and active colors
+  barTextColor: '#222b33',
+  barSelectedColor: '#4e56ea',
+  barBg: '#fff',
 
-//     // Form colors
-//     inputBg: '#fff',
-//     inputBorder: '#4e56ea',
-//     inputTextColor: '#222b33',
-//     inputBorderRadius: 0,
+  // Form colors
+  inputBg: '#fff',
+  inputBorder: '#4e56ea',
+  inputTextColor: '#222b33',
+  inputBorderRadius: 0,
 
-//     brandTitle: `Elements ${version}`,
-//     brandImage: logo,
-//   }),
-// })
+  brandTitle: `Elements ${version}`,
+  // brandImage: './public/reapitLogo.svg',
+})
