b67ba37e8d55af1e62348e865ebdf4b8ba0cfb80
Ashleigh Simonelli <ashleighsimonelli@gmail.com>
2024-03-05 15:30:45 +0000

    refactor: breadcrumbs to use anchors

--- src/components/breadcrumb/__styles__/index.ts
+++ src/components/breadcrumb/__styles__/index.ts
@@ -1,6 +1,7 @@
+import { unstyledAnchor } from '../../../styles/unstyled.anchor'
 import { styled } from '@linaria/react'
 
-export const ElBreadCrumbItem = styled.div`
+export const ElBreadCrumbItem = styled(unstyledAnchor)`
   border-radius: 0.5rem;
   font-size: var(--font-size-small);
   color: var(--intent-default);
