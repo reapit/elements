f7860f9ba4f948c78356cc5d05cbcb206f26ed0e
Ashleigh Simonelli <ashleighsimonelli@gmail.com>
2024-03-06 12:16:52 +0000

    refactor: added tagged component for layout components

--- src/components/layout/__styles__/index.ts
+++ src/components/layout/__styles__/index.ts
@@ -6,6 +6,7 @@ import { css } from '@linaria/core'
 export const elHasGreyBackground = css``
 export const elHasMaxWidth = css``
 
+/** @deprecated */
 export const ElMainContainer = styled.main`
   display: flex;
   flex-direction: column;
@@ -25,6 +26,26 @@ export const ElMainContainer = styled.main`
   }
 `
 
+export const elMainContainer = css`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 100%;
+  overflow-y: visible;
+  overflow-x: hidden;
+  background: var(--color-white);
+
+  &.${elHasGreyBackground} {
+    background: var(--color-grey-50);
+  }
+
+  &.${elHasMaxWidth} {
+    max-width: 1200px;
+    margin: 0 auto;
+  }
+`
+
+/** @deprecated */
 export const ElPageContainer = styled.section`
   display: block;
   padding: 0.5rem 1.25rem;
@@ -52,6 +73,34 @@ export const ElPageContainer = styled.section`
   }
 `
 
+export const elPageContainer = css`
+  display: block;
+  padding: 0.5rem 1.25rem;
+  width: 100%;
+  height: 100%;
+  overflow-y: scroll;
+  overflow-x: hidden;
+  background-color: var(--color-white);
+
+  ${isTablet} {
+    padding: 2.5rem 1.5rem;
+  }
+
+  &.${elHFull} {
+    height: 100%;
+  }
+
+  &.${elHasGreyBackground} {
+    background: var(--color-grey-50);
+  }
+
+  &.${elHasMaxWidth} {
+    max-width: 1200px;
+    margin: 0 auto;
+  }
+`
+
+/** @deprecated */
 export const ElSecondaryNavContainer = styled.aside`
   display: block;
   width: 14rem;
@@ -72,6 +121,27 @@ export const ElSecondaryNavContainer = styled.aside`
   }
 `
 
+export const elSecondaryNavContainer = css`
+  display: block;
+  width: 14rem;
+  flex: 0 0 14rem;
+  padding: 1.5rem;
+  overflow-y: scroll;
+  overflow-x: hidden;
+  scrollbar-width: none;
+  background: var(--color-white);
+  border-right: 1px solid var(--color-grey-50);
+
+  &::-webkit-scrollbar {
+    display: none;
+  }
+
+  ${isMobile} {
+    display: none;
+  }
+`
+
+/** @deprecated */
 export const ElMolecule = styled.div`
   display: block;
   padding: 0.75rem;
@@ -79,7 +149,23 @@ export const ElMolecule = styled.div`
   width: 100%;
 `
 
+/** @deprecated */
 export const ElFlexContainer = styled.div`
+  display: flex;
+  min-height: 0;
+  min-width: 0;
+
+  &.${elHasGreyBackground} {
+    background: var(--color-grey-50);
+  }
+
+  &.${elHasMaxWidth} {
+    max-width: 1200px;
+    margin: 0 auto;
+  }
+`
+
+export const elFlexContainer = css`
   display: flex;
   min-height: 0;
   min-width: 0;
