446b00f88249a9f5783ba49ad937f2923e461b06
Ashleigh Simonelli <ashleighsimonelli@gmail.com>
2024-03-06 16:21:51 +0000

    fix: remove deprecated element components

--- src/components/layout/__styles__/index.ts
+++ src/components/layout/__styles__/index.ts
@@ -6,26 +6,6 @@ import { css } from '@linaria/core'
 export const elHasGreyBackground = css``
 export const elHasMaxWidth = css``
 
-/** @deprecated */
-export const ElMainContainer = styled.main`
-  display: flex;
-  flex-direction: column;
-  width: 100%;
-  height: 100%;
-  overflow-y: visible;
-  overflow-x: hidden;
-  background: var(--color-white);
-
-  &.${elHasGreyBackground} {
-    background: var(--color-grey-50);
-  }
-
-  &.${elHasMaxWidth} {
-    max-width: 1200px;
-    margin: 0 auto;
-  }
-`
-
 export const elMainContainer = css`
   display: flex;
   flex-direction: column;
@@ -34,34 +14,6 @@ export const elMainContainer = css`
   overflow-y: visible;
   overflow-x: hidden;
   background: var(--color-white);
-
-  &.${elHasGreyBackground} {
-    background: var(--color-grey-50);
-  }
-
-  &.${elHasMaxWidth} {
-    max-width: 1200px;
-    margin: 0 auto;
-  }
-`
-
-/** @deprecated */
-export const ElPageContainer = styled.section`
-  display: block;
-  padding: 0.5rem 1.25rem;
-  width: 100%;
-  height: 100%;
-  overflow-y: scroll;
-  overflow-x: hidden;
-  background-color: var(--color-white);
-
-  ${isTablet} {
-    padding: 2.5rem 1.5rem;
-  }
-
-  &.${elHFull} {
-    height: 100%;
-  }
 
   &.${elHasGreyBackground} {
     background: var(--color-grey-50);
@@ -100,27 +52,6 @@ export const elPageContainer = css`
   }
 `
 
-/** @deprecated */
-export const ElSecondaryNavContainer = styled.aside`
-  display: block;
-  width: 14rem;
-  flex: 0 0 14rem;
-  padding: 1.5rem;
-  overflow-y: scroll;
-  overflow-x: hidden;
-  scrollbar-width: none;
-  background: var(--color-white);
-  border-right: 1px solid var(--color-grey-50);
-
-  &::-webkit-scrollbar {
-    display: none;
-  }
-
-  ${isMobile} {
-    display: none;
-  }
-`
-
 export const elSecondaryNavContainer = css`
   display: block;
   width: 14rem;
@@ -138,30 +69,6 @@ export const elSecondaryNavContainer = css`
 
   ${isMobile} {
     display: none;
-  }
-`
-
-/** @deprecated */
-export const ElMolecule = styled.div`
-  display: block;
-  padding: 0.75rem;
-  margin-bottom: 1.25rem;
-  width: 100%;
-`
-
-/** @deprecated */
-export const ElFlexContainer = styled.div`
-  display: flex;
-  min-height: 0;
-  min-width: 0;
-
-  &.${elHasGreyBackground} {
-    background: var(--color-grey-50);
-  }
-
-  &.${elHasMaxWidth} {
-    max-width: 1200px;
-    margin: 0 auto;
   }
 `
 
