3aa5c25aefc937c5bcb09a7513700a7c54fcd657
Kurt Doherty <kdoherty@reapit.com>
2024-08-01 13:53:31 +1000

    v5 component refactor: #136 TextArea (part 1)

--- src/components/textarea/textarea.stories.tsx
+++ src/components/textarea/textarea.stories.tsx
@@ -1,19 +1,72 @@
+import { Meta, StoryObj } from '@storybook/react'
 import { TextArea } from './index'
 
 export default {
   title: 'TextArea',
   component: TextArea,
+} as Meta<typeof TextArea>
+
+export const BasicUsage: StoryObj<typeof TextArea> = {
+  args: {
+    name: 'description',
+    placeholder: 'Description',
+  },
 }
 
-export const BasicUsage = {
-  render: ({}) => <TextArea type="text" placeholder="A placeholder" />,
+/**
+ * A TextArea can be invalid in three ways:
+ *
+ *  - Applying any of the standard [HTML form validation attributes](https://developer.mozilla.org/en-US/docs/Learn/Forms/Form_validation#using_built-in_form_validation) and providing a value that does not meet these constraints;
+ *  - Using custom [constraint validation](https://developer.mozilla.org/en-US/docs/Web/HTML/Constraint_validation); or,
+ *  - By explicitly setting `hasError` component prop to true (or the `el-text-area-has-error` class if using CSS only).
+ *
+ * The following example demonstrates a required text area that has no value, which is excersing option (1) above.
+ */
+export const InvalidValue: StoryObj<typeof TextArea> = {
+  args: {
+    placeholder: 'Type here...',
+    required: true,
+  },
 }
 
-export const TextAreaPlaceholder = {
-  render: ({}) => <TextArea type="text" placeholder="Hello" />,
-  name: 'Text Area placeholder',
+/**
+ * Text area's can automatically grow or shrink between a min and/or max row count. The min and max rows define the
+ * number of lines of text that should be visible within the text area. If the number of lines exceeds the specified
+ * maximum, the text area's content will overflow with a scrollbar.
+ *
+ * Note that the resizing behaviour is *not yet* available with CSS-only usage.
+ *
+ * This example demonstrates resizing behaviour for an *uncontrolled* text area.
+ */
+export const UncontrolledResizing: StoryObj<typeof TextArea> = {
+  args: {
+    defaultValue: '1\n2',
+    placeholder: 'Type here...',
+    maxRows: 5,
+    minRows: 1,
+  },
 }
 
-export const TextAreaError = {
-  render: ({}) => <TextArea type="text" hasError />,
+/**
+ * This next example demonstrates auto-sizing behaviour for a *controlled* text area. To change the value of the text
+ * area, you will need to use the `value` control.
+ */
+export const ControlledResizing: StoryObj<typeof TextArea> = {
+  args: {
+    placeholder: 'Type here...',
+    maxRows: 5,
+    minRows: 1,
+    value: '1\n2\n3',
+  },
+}
+
+/**
+ * Importantly, when an explicit row count is specified, no autosizing will occur, whether the text area's
+ * value is controlled or not.
+ */
+export const ExplicitRows: StoryObj<typeof TextArea> = {
+  args: {
+    placeholder: 'Type here...',
+    rows: 10,
+  },
 }
--- src/components/textarea/textarea.tsx
+++ src/components/textarea/textarea.tsx
@@ -1,22 +1,70 @@
-import React, { forwardRef, InputHTMLAttributes, LegacyRef } from 'react'
-import { ElTextArea, elTextAreaHasError } from './__styles__'
 import { cx } from '@linaria/core'
+import { ElTextArea, elTextAreaHasError, type TextAreaCSSProperties } from './styles'
+import { forwardRef } from 'react'
 
-export interface TextAreaProps extends InputHTMLAttributes<HTMLTextAreaElement> {
+import type { CSSProperties, TextareaHTMLAttributes } from 'react'
+
+// NOTE: We omit the `cols` prop because our text area should always grow to the width of its container.
+interface RestrictedTextareaHTMLAttributes extends Omit<TextareaHTMLAttributes<HTMLTextAreaElement>, 'cols'> {}
+
+export interface TextAreaProps extends RestrictedTextareaHTMLAttributes {
+  /**
+   * Indicates whether the text area has a form validation error or not. Provided as an escape-hatch
+   * for when you cannot rely on the [`:invalid` pseudo-class](https://developer.mozilla.org/en-US/docs/Web/CSS/:invalid)
+   * and need to control the error state manually.
+   */
   hasError?: boolean
+  /**
+   * The maximum number of rows to which the text area should be sized. Provides the upper bound
+   * for the text area to grow to, except where an explicit value for `rows` is defined.
+   *
+   * @default Infinity
+   */
+  maxRows?: number
+  /**
+   * The minimum number of rows to which the text area should be sized. Provides the lower bound
+   * for the text area to shrink to, except where an explicit value for `rows` is defined. The
+   * [MDN docs](https://developer.mozilla.org/en-US/docs/Web/HTML/Element/textarea#rows)
+   * highlight that the default number of rows is 2.
+   *
+   * @default 2
+   */
+  minRows?: number
+  /**
+   * The exact number of rows to which the text area should be sized. When provided, it overrides any
+   * other `minRows` or `maxRows` values. This also disables any auto-sizing behaviour for the text area.
+   */
+  rows?: number
 }
 
-export type TextAreaWrapped = React.ForwardRefExoticComponent<
-  TextAreaProps & React.RefAttributes<React.TextareaHTMLAttributes<HTMLTextAreaElement>>
->
-
-export const TextArea: TextAreaWrapped = forwardRef(({ hasError, ...rest }, ref) => {
-  return (
-    <ElTextArea
-      className={cx(hasError && elTextAreaHasError)}
-      aria-label="Text area input field"
-      {...rest}
-      ref={ref as unknown as LegacyRef<HTMLTextAreaElement>}
-    />
-  )
-})
+/**
+ * An (almost) standard HTML/JSX `<textarea>` for use in forms.
+ *
+ * Can automatically resize itself between a minimum and/or maximum number of lines of text (rows). This
+ * resizing behaviour is available for CSS-only consumers on Chrome and Edge. For browsers that do not yet
+ * support the [field-sizing](https://developer.mozilla.org/en-US/docs/Web/CSS/field-sizing) property, we
+ * fallback to a JS-based resizing solution that is only available to React-based consumers.
+ */
+export const TextArea = forwardRef<HTMLTextAreaElement, TextAreaProps>(
+  ({ className, defaultValue, hasError, maxRows = Infinity, minRows = 2, onChange, rows, value, ...rest }, ref) => {
+    return (
+      <ElTextArea
+        {...rest}
+        className={cx(hasError && elTextAreaHasError, className)}
+        defaultValue={defaultValue}
+        style={
+          {
+            // NOTE: `rows` takes precedence. If it's defined, the text area is effectively fixed-height.
+            // If it's not defined, we allow resizing to occur between the min and max rows.
+            '--textarea-max-rows': rows ?? maxRows,
+            '--textarea-min-rows': rows ?? minRows,
+          } satisfies TextAreaCSSProperties as CSSProperties
+        }
+        onChange={onChange}
+        ref={ref}
+        rows={rows}
+        value={value}
+      />
+    )
+  },
+)
