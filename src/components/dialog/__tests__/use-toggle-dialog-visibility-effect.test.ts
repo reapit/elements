750e4784a5bd56bd88d1cd95a0b20762eec515e3
Dimas M <96416644+ss-dimasm@users.noreply.github.com>
2024-10-24 08:23:08 +0700

    refactor: #144 - dialog component code clean up  (#177)
    * refactor: clean up dialog use-toggle-dialog-visibility-effect
    
    * refactor: rename dialog/style.ts to dialog/styles/ts
    
    * chore: add 'effect' word

--- src/components/dialog/__tests__/use-toggle-dialog-visibility-effect.test.ts
+++ src/components/dialog/__tests__/use-toggle-dialog-visibility-effect.test.ts
@@ -2,39 +2,63 @@ import { renderHook } from '@testing-library/react-hooks'
 
 import { useToggleDialogVisibilityEffect } from '../use-toggle-dialog-visibility-effect'
 import { RefObject } from 'react'
+import { act } from '@testing-library/react'
 
 describe('useToggleDialogVisibilityEffect', () => {
   const mockOnClose = jest.fn()
+  const abort = jest.fn()
+
   const mockUseRef = {
     current: {
       showModal: jest.fn(),
       addEventListener: jest.fn(),
-      removeEventListener: jest.fn(),
       close: jest.fn(),
     },
   } as unknown as RefObject<HTMLDialogElement>
+
+  beforeAll(() => {
+    jest.spyOn(AbortController.prototype, 'abort').mockImplementation(abort) as any
+  })
 
   afterEach(() => {
     jest.clearAllMocks()
   })
 
   it('should trigger the some methods when isOpen is true', () => {
-    renderHook(() => useToggleDialogVisibilityEffect({ ref: mockUseRef, isOpen: true, onClose: mockOnClose }))
+    const render = renderHook(() =>
+      useToggleDialogVisibilityEffect({ ref: mockUseRef, isOpen: true, onClose: mockOnClose }),
+    )
 
     expect(mockUseRef.current?.showModal).toHaveBeenCalled()
     expect(mockUseRef.current?.addEventListener).toHaveBeenCalled()
 
     expect(mockUseRef.current?.close).not.toHaveBeenCalled()
-    expect(mockUseRef.current?.removeEventListener).not.toHaveBeenCalled()
+    expect(abort).not.toHaveBeenCalled()
+
+    act(() => {
+      render.unmount()
+    })
+
+    expect(abort).toHaveBeenCalledTimes(1)
   })
 
   it('should trigger the some methods when isOpen is false', () => {
-    renderHook(() => useToggleDialogVisibilityEffect({ ref: mockUseRef, isOpen: false, onClose: mockOnClose }))
+    abort.mockClear()
+
+    const hook = renderHook(() =>
+      useToggleDialogVisibilityEffect({ ref: mockUseRef, isOpen: false, onClose: mockOnClose }),
+    )
 
     expect(mockUseRef.current?.showModal).not.toHaveBeenCalled()
     expect(mockUseRef.current?.addEventListener).not.toHaveBeenCalled()
 
-    expect(mockUseRef.current?.close).toHaveBeenCalled()
-    expect(mockUseRef.current?.removeEventListener).toHaveBeenCalled()
+    expect(abort).toHaveBeenCalledTimes(1)
+    expect(mockUseRef.current?.close).toHaveBeenCalledTimes(1)
+
+    act(() => {
+      hook.unmount()
+    })
+
+    expect(abort).toHaveBeenCalledTimes(2)
   })
 })
