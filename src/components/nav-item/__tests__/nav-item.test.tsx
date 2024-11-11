3c3b2a5f98592ec55f9e7f7262c53fc3f470034e
Nur M <106480336+nurm717123@users.noreply.github.com>
2024-11-11 11:16:49 +0700

    feat: #195 new app-bar (nav item) (#200)
    * feat: init new nav-item

---
+++ src/components/nav-item/__tests__/nav-item.test.tsx
@@ -0,0 +1,38 @@
+import { render } from '@testing-library/react'
+import { NavItem } from '../nav-item'
+
+describe('NavItem', () => {
+  it('should render the component as HTMLAnchorElement with default state', () => {
+    expect(render(<NavItem href="#">Label</NavItem>).asFragment()).toMatchSnapshot()
+  })
+
+  it('should render the component as HTMLAnchorElement with active state', () => {
+    expect(
+      render(
+        <NavItem href="#" isActive>
+          Label
+        </NavItem>,
+      ).asFragment(),
+    ).toMatchSnapshot()
+  })
+
+  it('should render the component as HTMLButtonElement with default state', () => {
+    expect(
+      render(
+        <NavItem onClick={jest.fn()} isActive>
+          Label
+        </NavItem>,
+      ).asFragment(),
+    ).toMatchSnapshot()
+  })
+
+  it('should render the component as HTMLButtonElement with active state', () => {
+    expect(
+      render(
+        <NavItem onClick={jest.fn()} isActive>
+          Label
+        </NavItem>,
+      ).asFragment(),
+    ).toMatchSnapshot()
+  })
+})
