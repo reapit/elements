08d221aea772078b3e349600859ad07c1ad905b1
nurm717123 <nur@softwareseni.com>
2024-11-15 10:15:48 +0700

    backup

--- src/components/avatar-rectangle/avatar-rectangle.tsx
+++ src/components/avatar-rectangle/avatar-rectangle.tsx
@@ -10,20 +10,32 @@ import {
 export interface AvatarRectangle extends HTMLAttributes<HTMLSpanElement> {
   variant: 'residential' | 'commercial'
   size: 'medium' | 'small'
+  src?: string
+  alt?: string
 }
 
-export const AvatarRectangle: FC<AvatarRectangle> = ({ size, variant, children, ...props }) => {
+export const AvatarRectangle: FC<AvatarRectangle> = ({ size, variant, src, alt, ...props }) => {
   return (
-    <ElAvatarRectangle {...props} data-size={size} data-variant={variant} data-placeholder={Boolean(children) == false}>
-      {children ? (
-        children
+    <ElAvatarRectangle
+      role="img"
+      {...props}
+      data-size={size}
+      data-variant={variant}
+      data-placeholder={Boolean(src) == false}
+    >
+      {src ? (
+        <img src={src} alt={alt} />
       ) : variant === 'commercial' ? (
         <ElAvatarRectCommercialPlaceholder />
       ) : (
         <ElAvatarRectangleResidentialPlaceholder />
       )}
       {variant === 'commercial' &&
-        (size === 'small' ? <ElAvatarRectBottomPlaceholderSmall /> : <ElAvatarRectBottomPlaceholder />)}
+        (size === 'small' ? (
+          <ElAvatarRectBottomPlaceholderSmall aria-hidden="true" />
+        ) : (
+          <ElAvatarRectBottomPlaceholder aria-hidden="true" />
+        ))}
     </ElAvatarRectangle>
   )
 }
