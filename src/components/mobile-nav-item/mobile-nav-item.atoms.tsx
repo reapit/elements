f8ea4d85d8815b8063f5bf7380d0667c47999618
Dimas M <96416644+ss-dimasm@users.noreply.github.com>
2024-11-26 13:57:05 +0700

    feat: #206 initiate mobile navigation item (#209)
    * feat: #206 initiate mobile navigation item
    
    * refactor: resolve PR feedback
    
    * chore: update snapshot

---
+++ src/components/mobile-nav-item/mobile-nav-item.atoms.tsx
@@ -0,0 +1,108 @@
+import { useState } from 'react'
+import type { FC, ReactNode, MouseEventHandler } from 'react'
+
+import { useId } from '../../storybook/random-id'
+
+import { Icon } from '../icon'
+
+import {
+  ElMobileNavItemAnchor,
+  ElMobileNavItemBadge,
+  ElMobileNavItemButton,
+  ElMobileNavItemContent,
+  ElMobileNavItemExpanderButton,
+  ElMobileNavItemListItem,
+  ElMobileNavSubItemUnorderedList,
+} from './styles'
+
+type CommonMobileNavItemProps = {
+  label: string
+  isActive?: boolean
+  hasBadge?: boolean
+}
+
+interface MobileNavItemSimpleAsButtonElementProps extends CommonMobileNavItemProps {
+  onClick: MouseEventHandler<HTMLButtonElement>
+  href?: never
+  children?: never
+}
+
+interface MobileNavItemSimpleAsAnchorElementProps extends CommonMobileNavItemProps {
+  href: string
+  onClick?: never
+  children?: never
+}
+
+export type MobileNavItemSimpleProps = MobileNavItemSimpleAsButtonElementProps | MobileNavItemSimpleAsAnchorElementProps
+
+const isMobileNavItemSimpleAsButtonElement = (
+  props: MobileNavItemSimpleProps,
+): props is MobileNavItemSimpleAsButtonElementProps => {
+  return 'onClick' in props
+}
+
+export const MobileNavItemSimple: FC<MobileNavItemSimpleProps> = (props) => {
+  if (isMobileNavItemSimpleAsButtonElement(props)) {
+    const { onClick, isActive, label, hasBadge, ...rest } = props ?? {}
+
+    return (
+      <ElMobileNavItemListItem {...rest} aria-label={label}>
+        <ElMobileNavItemButton onClick={onClick} aria-current={isActive ? 'true' : undefined}>
+          <ElMobileNavItemContent>
+            {label}
+            {hasBadge && <ElMobileNavItemBadge />}
+          </ElMobileNavItemContent>
+        </ElMobileNavItemButton>
+      </ElMobileNavItemListItem>
+    )
+  } else {
+    const { href, isActive, label, hasBadge, ...rest } = props ?? {}
+
+    return (
+      <ElMobileNavItemListItem {...rest} aria-label={label}>
+        <ElMobileNavItemAnchor href={href} aria-current={isActive ? 'page' : undefined}>
+          <ElMobileNavItemContent>
+            {label}
+            {hasBadge && <ElMobileNavItemBadge />}
+          </ElMobileNavItemContent>
+        </ElMobileNavItemAnchor>
+      </ElMobileNavItemListItem>
+    )
+  }
+}
+
+export interface MobileNavItemExpandableProps extends CommonMobileNavItemProps {
+  children: ReactNode
+  href?: never
+  onClick?: never
+}
+
+export const MobileNavItemExpandable: FC<MobileNavItemExpandableProps> = (props) => {
+  const [isExpanded, setIsExpanded] = useState(Boolean(props?.isActive))
+  const panelId = useId()
+
+  const { isActive, label, hasBadge, children, ...rest } = props ?? {}
+
+  return (
+    <ElMobileNavItemListItem {...rest} data-is-expanded={isExpanded} aria-label={label}>
+      <ElMobileNavItemExpanderButton
+        type="button"
+        aria-expanded={isExpanded ?? isActive}
+        aria-current={isActive ? 'true' : undefined}
+        aria-controls={panelId}
+        onClick={() => setIsExpanded((prev) => !prev)}
+      >
+        <ElMobileNavItemContent>
+          {label}
+          {hasBadge && <ElMobileNavItemBadge />}
+        </ElMobileNavItemContent>
+
+        <Icon icon={isExpanded ? 'chevronUp' : 'chevronDown'} fontSize="16px" />
+      </ElMobileNavItemExpanderButton>
+
+      <ElMobileNavSubItemUnorderedList id={panelId} aria-hidden={!isExpanded}>
+        {children}
+      </ElMobileNavSubItemUnorderedList>
+    </ElMobileNavItemListItem>
+  )
+}
