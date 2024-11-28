c7885a6d6e7dd1f37bfefcaf2a0d57720a487d28
nurm717123 <nur@softwareseni.com>
2024-11-28 14:22:39 +0700

    test: fix test and update snapshot

--- src/components/top-bar/__test__/top-bar.test.tsx
+++ src/components/top-bar/__test__/top-bar.test.tsx
@@ -7,8 +7,7 @@ import { Menu } from '../../menu'
 import { NavDropdownButton } from '../../nav-dropdown-button'
 import { NavIconItem } from '../../nav-icon-item'
 import { NavSearchButton } from '../../nav-search-button/nav-search-button'
-import { AvatarButton } from '../../avatar-button'
-import { elTopBarAvatar } from '../styles'
+import { ElTopBarProfile } from '../styles'
 
 vi.mock('../../icon', () => ({
   Icon: vi.fn(() => <div data-testid="icon" />),
@@ -20,7 +19,6 @@ describe('TopBar Snapshot', () => {
       <TopBar>
         <TopBar.MainNav>
           <NavItem href="/">Button 1</NavItem>
-
           <CSSContainerQuery condition={'(width < 1000px)'}>
             <NavItem href="/">Button 2</NavItem>
           </CSSContainerQuery>
@@ -38,7 +36,6 @@ describe('TopBar Snapshot', () => {
           </CSSContainerQuery>
         </TopBar.MainNav>
         <TopBar.Search>
-          <NavIconItem icon={<Icon icon="search" />} aria-label="nav-icon-item-example" />
           <NavSearchButton />
         </TopBar.Search>
 
@@ -49,7 +46,7 @@ describe('TopBar Snapshot', () => {
           <NavIconItem aria-label="mobile secondary nav trigger" icon={<Icon icon="menu" />} />
         </TopBar.MobileNav>
 
-        <AvatarButton className={elTopBarAvatar} label="AD" />
+        <ElTopBarProfile label="AD" />
       </TopBar>,
     )
     expect(asFragment()).toMatchSnapshot()
