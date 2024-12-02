b3f53615840e14afa0be1e9c7f9a0514b18f8e68
Kushal Solanki <kushal.solanki@agentbox.com.au>
2024-12-03 10:33:52 +1100

    Fix prettier issue

--- src/components/skeleton/skeleton.tsx
+++ src/components/skeleton/skeleton.tsx
@@ -7,6 +7,6 @@ interface SkeletonProps extends HTMLAttributes<HTMLSpanElement> {
   borderRadius?: string
 }
 
-export const Skeleton: React.FC<SkeletonProps> = ({ height="1rem", width="10rem", borderRadius, ...rest }) => {
+export const Skeleton: React.FC<SkeletonProps> = ({ height = '1rem', width = '10rem', borderRadius, ...rest }) => {
   return <ElSkeleton style={{ height: height, width: width, borderRadius: borderRadius }} {...rest} />
 }
