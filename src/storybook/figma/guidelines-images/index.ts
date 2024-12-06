d19517e5555b7bf44e8c0c0c6b8425e7073d1d25
nurm717123 <nur@softwareseni.com>
2024-12-06 13:57:31 +0700

    chore: update top-bar mdx docs

--- src/storybook/figma/guidelines-images/index.ts
+++ src/storybook/figma/guidelines-images/index.ts
@@ -1,7 +1,5 @@
 import Accordion from './accordion.svg'
 import ButtonGroup from './button-group.svg'
-import TopBar from './top-bar.svg'
 
 export { Accordion }
-export { TopBar }
 export { ButtonGroup }
