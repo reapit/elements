300eab6df60c17fe40ae5b3deca64d80ef48db05
nurm717123 <nur@softwareseni.com>
2024-12-06 14:23:47 +0700

    chore: add todo to delete design guidelines svg

--- src/storybook/figma/guidelines-images/index.ts
+++ src/storybook/figma/guidelines-images/index.ts
@@ -1,5 +1,6 @@
 import Accordion from './accordion.svg'
 import ButtonGroup from './button-group.svg'
 
+// TODO: can delete this, as we're not planning to use Design Guidelines docs as a documentation for developer
 export { Accordion }
 export { ButtonGroup }
