d5c5f84bb8381e357efc2b085674230182c61767
ss-dimasm <dimas.m@softwareseni.com>
2024-12-16 13:28:02 +0700

    chore: remove fallback css variables for `MobileNavMenu` and amend the stories

--- src/components/mobile-nav-item/mobile-nav-item.stories.tsx
+++ src/components/mobile-nav-item/mobile-nav-item.stories.tsx
@@ -19,19 +19,19 @@ export default meta
 type Story = StoryObj<typeof meta>
 
 export const Simple: Story = {
-  render: () => <MobileNavItem label="Item 1" href="#item-1" />,
+  render: ({}) => <MobileNavItem label="Item 1" href="#item-1" />,
 }
 
-export const SimpleWithBadge: Story = {
-  render: () => <MobileNavItem label="Item 1" href="#item-1" hasBadge />,
+export const WithBadge: Story = {
+  render: ({}) => <MobileNavItem label="Item 1" href="#item-1" hasBadge />,
 }
 
-export const SimpleWithActiveState: Story = {
-  render: () => <MobileNavItem label="Item 1" href="#item-1" isActive />,
+export const WithActiveState: Story = {
+  render: ({}) => <MobileNavItem label="Item 1" href="#item-1" isActive />,
 }
 
 export const Expandable: Story = {
-  render: () => (
+  render: ({}) => (
     <MobileNavItem label="Item 2" hasBadge>
       <MobileNavItem label="Sub Item 2.1" href="#item-2.1" />
       <MobileNavItem label="Sub Item 2.2" onClick={action('onClick')} hasBadge />
@@ -41,7 +41,7 @@ export const Expandable: Story = {
 }
 
 export const DefaultExpanded: Story = {
-  render: () => (
+  render: ({}) => (
     <MobileNavItem label="Item 3" isActive>
       <MobileNavItem label="Sub Item 3.1" href="#item-3.1" />
       <MobileNavItem label="Sub Item 3.2" onClick={action('onClick')} isActive />
