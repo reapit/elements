2970af67b430da3952c98f106c7ef733a4fb456d
foxy <foxy@foxy.com>
2025-01-14 07:27:31 +0700

    chore: #272-v5-label-component-deprecated-existing add detail in deprecated info

--- src/components/deprecated-label/__styles__/index.ts
+++ src/components/deprecated-label/__styles__/index.ts
@@ -1,6 +1,8 @@
 import { styled } from '@linaria/react'
 
-/** @deprecated */
+/**
+ * @deprecated This style is deprecated and will be removed in future releases.
+ */
 export const ElDeprecatedLabel = styled.label`
   font-size: var(--font-size-small);
   color: var(--neutral-500);
