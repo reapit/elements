c4a4b237064397d4524dc801c23c079b23fefe16
nurm717123 <nur@softwareseni.com>
2025-01-24 15:20:06 +0700

    resolve feedback: use discriminated union props

--- src/components/tag/tag.tsx
+++ src/components/tag/tag.tsx
@@ -1,9 +1,16 @@
 import { cx } from '@linaria/core'
 import { elTag, ElTagGroup } from './styles'
+import type { HTMLAttributes } from 'react'
 
-export interface TagProps extends React.HTMLAttributes<HTMLLIElement | HTMLSpanElement> {
-  as?: 'span' | 'li'
+interface TagAsSpanProps extends HTMLAttributes<HTMLSpanElement> {
+  as: 'span'
 }
+
+interface TagAsListItemProps extends HTMLAttributes<HTMLLIElement> {
+  as?: 'li'
+}
+
+export type TagProps = TagAsSpanProps | TagAsListItemProps
 
 export const Tag: React.FC<TagProps> = ({ children, as: Element = 'li', ...props }) => {
   return (
