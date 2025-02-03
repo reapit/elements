9e8693cb0fb3b2c85a583f8d53b48995d344d5a5
foxy <foxy@foxy.com>
2025-02-03 07:46:12 +0700

    feat: #272 v5-label-text component add more documented view

--- src/components/label-text/label-text.stories.tsx
+++ src/components/label-text/label-text.stories.tsx
@@ -1,4 +1,4 @@
-import { ElLabelText } from './index'
+import { ElLabelText, LabelText } from './index'
 
 export default {
   title: 'Components/LabelText',
@@ -6,5 +6,24 @@ export default {
 }
 
 export const BasicUsage = {
-  render: ({}) => <ElLabelText>I&apos;m a label for a form input or something else</ElLabelText>,
+  render: ({ }) => (
+    <>
+      <LabelText>I&apos;m a label for a form input or something else</LabelText>
+      <LabelText>example labeltext default</LabelText>
+      <br />
+      <LabelText size="small">example labeltext strong small</LabelText>
+      <br />
+      <LabelText variant="strong" size="medium">
+        example labeltext strong medium
+      </LabelText>
+      <br />
+      <LabelText variant="soft" size="small">
+        example labeltext soft small
+      </LabelText>
+      <br />
+      <LabelText variant="soft" isRequired>
+        example labeltext soft isRequired
+      </LabelText>
+    </>
+  ),
 }
