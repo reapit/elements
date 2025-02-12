7afa360174360d02121b23b4db226911080981be
foxy <foxy@foxy.com>
2025-02-12 14:31:26 +0700

    feat: #272 v5-label-text component fix linter

--- src/components/table/table/table.tsx
+++ src/components/table/table/table.tsx
@@ -1,7 +1,9 @@
-import React, { FC, TableHTMLAttributes } from 'react'
+import React, { FC, ReactNode, TableHTMLAttributes } from 'react'
 import { ElTable } from './styles'
 
-export interface TableProps extends TableHTMLAttributes<HTMLTableElement> {}
+export interface TableProps extends TableHTMLAttributes<HTMLTableElement> {
+  children: ReactNode
+}
 
 export const Table: FC<TableProps> = ({ children, ...rest }) => {
   return <ElTable {...rest}>{children}</ElTable>
