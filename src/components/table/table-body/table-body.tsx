54033f6ad9c2bf977bf0bc6e7292dd36b67861a6
foxy <foxy@foxy.com>
2025-02-20 06:19:57 +0700

    feat: 218-v5-table-component update documentation description

--- src/components/table/table-body/table-body.tsx
+++ src/components/table/table-body/table-body.tsx
@@ -5,6 +5,24 @@ export interface TableBodyProps extends TableHTMLAttributes<HTMLTableSectionElem
   children: ReactNode
 }
 
+/**
+ * The `TableBody` component is an essential part of a table structure,
+ * serving as a container for table rows (`TableRow`).
+ * Each row inside a `TableBody` typically consists of one or more `TableCell` components.
+ *
+ * ## When to Use
+ *
+ * - To display structured data inside a table.
+ * - When working with dynamic data sets that require programmatically rendered rows.
+ * - As part of a table’s body section, following the `TableHead` component.
+ *
+ * ## Basic Structure
+ *
+ * A typical table implementation consists of:
+ * - `TableHead`: Defines column headers.
+ * - `TableBody`: Contains the main data rows.
+ */
+
 export const TableBody: FC<TableBodyProps> = ({ children, ...rest }) => {
   return <ElTableBody {...rest}>{children}</ElTableBody>
 }
