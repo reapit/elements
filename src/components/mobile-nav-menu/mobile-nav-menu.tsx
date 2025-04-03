7d03de93ef82263386e7024e02fec56bb36fb767
ss-dimasm <dimas.m@softwareseni.com>
2025-04-03 13:52:15 +0700

    refactor(mobile-nav-menu): add keyboard support

--- src/components/mobile-nav-menu/mobile-nav-menu.tsx
+++ src/components/mobile-nav-menu/mobile-nav-menu.tsx
@@ -1,4 +1,4 @@
-import { FC, ReactNode, useRef } from 'react'
+import { FC, KeyboardEventHandler, ReactNode, useRef } from 'react'
 import { MobileNavItem } from '../mobile-nav-item'
 import { MobileNavMenuHeader } from './mobile-nav-menu.atoms'
 import { ElMobileNavMenu, ElMobileNavMenuContent, ElMobileNavMenuItemGroup } from './styles'
@@ -22,7 +22,54 @@ const MobileNavMenu: MobileNavMenuFC = ({ children, isOpen, onClose }) => {
 
   useToggleDialogVisibilityEffect({ isOpen, ref, onClose })
 
-  return <ElMobileNavMenu ref={ref}>{children}</ElMobileNavMenu>
+  const handleOnKeyDown: KeyboardEventHandler<HTMLDialogElement> = (e) => {
+    const container = e.currentTarget
+
+    /**
+     * NOTE:  handle the keyboard navigation for the mobile nav menu
+     *
+     * This will collect all the clickable items in the mobile nav menu, and
+     * will handle the keyboard navigation for the mobile nav menu
+     */
+    const clickableItems = container?.querySelectorAll(
+      'ul:not([aria-hidden="true"]):not([aria-hidden="false"]) > li > a, ul[aria-hidden="false"] > li > a, button',
+    ) as NodeListOf<HTMLElement>
+
+    let currentIndex = -1
+    clickableItems.forEach((item, index) => {
+      if (item === document.activeElement) {
+        currentIndex = index
+      }
+    })
+
+    switch (e.key) {
+      case 'ArrowDown': {
+        e.preventDefault()
+        const nextItem = clickableItems[(currentIndex + 1) % clickableItems.length]
+        nextItem.focus()
+        break
+      }
+      case 'ArrowUp': {
+        e.preventDefault()
+        const prevItem = clickableItems[(currentIndex - 1 + clickableItems.length) % clickableItems.length]
+        prevItem.focus()
+        break
+      }
+      case 'Enter':
+      case ' ': {
+        e.preventDefault()
+        const currentItem = clickableItems[currentIndex] as HTMLAnchorElement | HTMLButtonElement
+        currentItem.click()
+        break
+      }
+    }
+  }
+
+  return (
+    <ElMobileNavMenu ref={ref} onKeyDown={handleOnKeyDown}>
+      {children}
+    </ElMobileNavMenu>
+  )
 }
 
 MobileNavMenu.ItemGroup = ElMobileNavMenuItemGroup
