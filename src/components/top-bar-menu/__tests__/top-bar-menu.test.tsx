1f6cc65f119bc061a644e695cce95326a05cbec1
ss-dimasm <dimas.m@softwareseni.com>
2025-04-15 11:50:33 +0700

    feat(top-bar-menu): add keyboard interaction

--- src/components/top-bar-menu/__tests__/top-bar-menu.test.tsx
+++ src/components/top-bar-menu/__tests__/top-bar-menu.test.tsx
@@ -1,5 +1,6 @@
-import { render } from '@testing-library/react'
+import { fireEvent, render } from '@testing-library/react'
 import { TopBarMenu } from '../top-bar-menu'
+import { TopBar } from '#src/components/top-bar/top-bar'
 
 vi.mock('../../icon', () => ({
   Icon: () => <svg data-name="mocked" />,
@@ -26,5 +27,64 @@ describe('TopbarMenu', () => {
         </TopBarMenu>,
       ).asFragment(),
     ).toMatchSnapshot()
+  })
+
+  it('should handle the keyboard navigation properly', () => {
+    const mockOnButtonClick = vi.fn()
+    const mockOnKeyDown = vi.fn()
+
+    render(
+      <TopBarMenu isOpen onKeyDown={mockOnKeyDown}>
+        <TopBarMenu.Body>
+          <TopBarMenu.List>
+            <TopBarMenu.Group isActive={false} label="Main Nav Item 1">
+              <TopBarMenu.Item label="Sub Nav Item 1" href="#item-1.1" />
+              <TopBarMenu.Item label="Sub Nav Item 2" href="#item-1.2" />
+            </TopBarMenu.Group>
+            <TopBarMenu.Item label="Main Nav Item 2" onClick={mockOnButtonClick} />
+          </TopBarMenu.List>
+        </TopBarMenu.Body>
+      </TopBarMenu>,
+    )
+
+    const getItems = () =>
+      document?.querySelectorAll(
+        'ul:not([aria-hidden="true"]):not([aria-hidden="false"]) > li > a, ul[aria-hidden="false"] > li > a, button',
+      ) as NodeListOf<HTMLElement>
+
+    const initialItems = getItems()
+    expect(initialItems).toHaveLength(2)
+
+    initialItems[0].focus()
+
+    // NOTE: proving the sub items are unaccessible while the parent item is collapsed
+    fireEvent.keyDown(document.activeElement!, { key: 'ArrowDown' })
+    expect(document.activeElement).toBe(initialItems[1])
+    fireEvent.keyDown(document.activeElement!, { key: 'ArrowUp' })
+    expect(document.activeElement).toBe(initialItems[0])
+    fireEvent.keyDown(document.activeElement!, { key: 'ArrowRight' })
+    expect(document.activeElement).toBe(initialItems[0])
+
+    // NOTE: ensure the sub items are accessible while the parent item expanded
+    const expandedItems = getItems()
+    expect(expandedItems).toHaveLength(4)
+    fireEvent.keyDown(document.activeElement!, { key: 'ArrowDown' })
+    expect(document.activeElement).toBe(expandedItems[1])
+    fireEvent.keyDown(document.activeElement!, { key: 'ArrowDown' })
+    expect(document.activeElement).toBe(expandedItems[2])
+    fireEvent.keyDown(document.activeElement!, { key: 'ArrowDown' })
+    expect(document.activeElement).toBe(expandedItems[3])
+
+    // NOTE: move back to the first item
+    fireEvent.keyDown(document.activeElement!, { key: 'ArrowDown' })
+    expect(document.activeElement).toBe(expandedItems[0])
+
+    // NOTE: asserting the expand and collapse functionality with across key strokes
+    fireEvent.keyDown(document.activeElement!, { key: 'ArrowLeft' })
+    expect(getItems()).toHaveLength(2)
+    fireEvent.keyDown(document.activeElement!, { key: 'Enter' })
+    expect(getItems()).toHaveLength(4)
+    fireEvent.keyDown(document.activeElement!, { key: ' ' })
+    expect(getItems()).toHaveLength(2)
   })
 })
--- src/components/top-bar-menu/top-bar-menu.tsx
+++ src/components/top-bar-menu/top-bar-menu.tsx
@@ -1,4 +1,4 @@
-import { FC, ReactNode, useRef } from 'react'
+import { DialogHTMLAttributes, FC, KeyboardEventHandler, ReactNode, useRef } from 'react'
 
 import { useToggleDialogVisibilityEffect } from '../dialog'
 import { TopBarMenuItem } from '../top-bar-menu-item'
@@ -8,7 +8,7 @@ import { TopBarMenuHeader } from './top-bar-menu.atoms'
 import { ElTopBarMenu, ElTopBarMenuBody, ElTopBarMenuList } from './styles'
 import { TopBarMenuProvider } from './top-bar-menu-context'
 
-export interface TopBarMenuProps {
+export interface TopBarMenuProps extends DialogHTMLAttributes<HTMLDialogElement> {
   isOpen: boolean
   children: ReactNode
   onClose?: VoidFunction
@@ -22,14 +22,73 @@ type TopBarMenuFC = FC<TopBarMenuProps> & {
   Item: typeof TopBarMenuItem
 }
 
-const TopBarMenu: TopBarMenuFC = ({ children, isOpen, onClose }) => {
+const TopBarMenu: TopBarMenuFC = ({ children, isOpen, onClose, ...rest }) => {
   const ref = useRef<HTMLDialogElement>(null)
 
   useToggleDialogVisibilityEffect({ isOpen, ref, onClose })
 
+  const handleOnKeyDown: KeyboardEventHandler<HTMLDialogElement> = (e) => {
+    const container = e.currentTarget
+
+    /**
+     * NOTE:  handle the keyboard navigation for the mobile nav menu
+     *
+     * This will collect all the clickable items in the mobile nav menu, and
+     * will handle the keyboard navigation for the mobile nav menu
+     */
+    const clickableItems = container?.querySelectorAll(
+      'ul:not([aria-hidden="true"]):not([aria-hidden="false"]) > li > a, ul[aria-hidden="false"] > li > a, button',
+    ) as NodeListOf<HTMLElement>
+
+    let currentIndex = -1
+    clickableItems.forEach((item, index) => {
+      if (item === document.activeElement) {
+        currentIndex = index
+      }
+    })
+
+    switch (e.key) {
+      case 'ArrowDown': {
+        e.preventDefault()
+        const nextItem = clickableItems[(currentIndex + 1) % clickableItems.length]
+        nextItem.focus()
+        break
+      }
+      case 'ArrowUp': {
+        e.preventDefault()
+        const prevItem = clickableItems[(currentIndex - 1 + clickableItems.length) % clickableItems.length]
+        prevItem.focus()
+        break
+      }
+      case 'ArrowRight':
+      case 'ArrowLeft': {
+        e.preventDefault()
+        const currentItem = clickableItems[currentIndex]
+        if (!currentItem.hasAttribute('aria-expanded')) break
+
+        if (e.key === 'ArrowRight' && currentItem.getAttribute('aria-expanded') === 'true') break
+        else if (e.key === 'ArrowLeft' && currentItem.getAttribute('aria-expanded') === 'false') break
+
+        currentItem.click()
+        break
+      }
+      case 'Enter':
+      case ' ': {
+        e.preventDefault()
+        const currentItem = clickableItems[currentIndex]
+        currentItem.click()
+        break
+      }
+    }
+
+    rest?.onKeyDown?.(e)
+  }
+
   return (
     <TopBarMenuProvider onClose={onClose}>
-      <ElTopBarMenu ref={ref}>{children}</ElTopBarMenu>
+      <ElTopBarMenu ref={ref} {...rest} onKeyDown={handleOnKeyDown}>
+        {children}
+      </ElTopBarMenu>
     </TopBarMenuProvider>
   )
 }
