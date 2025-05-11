1bcefe21d1c0c7fa89c7e50f761e05a291d12ca9
Kurt Doherty <kdoherty@reapit.com>
2025-05-12 09:54:33 +1000

    Drawer part 2, DrawerBody

---
+++ src/components/drawer/body/styles.ts
@@ -0,0 +1,8 @@
+import { styled } from '@linaria/react'
+
+export const ElDrawerBody = styled.div`
+  background: var(--fill-white);
+  flex-grow: 1;
+  grid-area: body;
+  padding: var(--spacing-6) var(--spacing-7);
+`
