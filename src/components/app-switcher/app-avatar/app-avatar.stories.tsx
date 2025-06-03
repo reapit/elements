42b65e54a1fddb8c0b98d178f13bfbce37ddaab2
Kurt Doherty <kdoherty@reapit.com>
2025-06-04 09:09:02 +1000

    feat: #428 Update app avatar

--- src/components/app-switcher/app-avatar/app-avatar.stories.tsx
+++ src/components/app-switcher/app-avatar/app-avatar.stories.tsx
@@ -1,26 +1,19 @@
-import type { Meta, StoryObj } from '@storybook/react'
-import { AppName } from '../appNames'
-import AppAvatar from './app-avatar'
+import { AppAvatar } from './app-avatar'
+import { productConfigs } from '../config'
 
-const appNames: AppName[] = [
-  'reapitPM',
-  'reapitSales',
-  'reapitLetting',
-  'reapitForms',
-  'reapitWebsites',
-  'reapitProposals',
-  'keyWhere',
-  'autoResponder',
-] as const
+import type { Meta, StoryObj } from '@storybook/react'
+import type { SupportedProductId } from '../config'
+
+const productIds = Object.keys(productConfigs) as SupportedProductId[]
 
 const meta: Meta<typeof AppAvatar> = {
   title: 'Components/AppSwitcher/AppAvatar',
   component: AppAvatar,
   argTypes: {
-    appName: {
+    productId: {
       control: { type: 'select' },
       description: 'Visual style of the avatars',
-      options: appNames,
+      options: productIds,
     },
   },
 } satisfies Meta<typeof AppAvatar>
@@ -29,16 +22,52 @@ export default meta
 
 type Story = StoryObj<typeof meta>
 
-export const BasicUsageWhenTheAvatarIsAccessible: Story = {
+/**
+ * When the user has access to the product, the avatar will appear in the primary colour.
+ */
+export const Example: Story = {
   args: {
-    appName: 'reapitPM',
+    productId: 'consoleCloud',
     hasAccess: true,
   },
 }
 
-export const BasicUsageWhenTheAvatarIsInaccessible: Story = {
+/**
+ * When the user does not have access to the product, the avatar will be greyed out.
+ */
+export const Inaccessible: Story = {
   args: {
-    appName: 'reapitPM',
+    ...Example.args,
     hasAccess: false,
   },
+}
+
+/**
+ * All supported products are displayed here. Each product has two avatars: one for when the user has access
+ * and one for when they do not.
+ */
+export const AllProducts: Story = {
+  argTypes: {
+    productId: {
+      control: false,
+    },
+  },
+  render: (_args) => (
+    <div
+      style={{
+        alignItems: 'center',
+        display: 'grid',
+        color: '#FA00FF',
+        fontSize: 'var(--font-base-regular-size)',
+        gridTemplateColumns: 'max-content min-content min-content',
+        gap: 'var(--spacing-5)',
+      }}
+    >
+      {productIds.map((productId) => [
+        <p>{productConfigs[productId].appName}</p>,
+        <AppAvatar productId={productId} hasAccess={true} />,
+        <AppAvatar productId={productId} hasAccess={false} />,
+      ])}
+    </div>
+  ),
 }
