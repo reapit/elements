904d0687afdd1d32ddf4656cda812e9e890683ce
Kurt Doherty <kdoherty@reapit.com>
2025-06-04 09:07:42 +1000

    feat: #428 Update menu groups and their related context

--- src/components/app-switcher/explore-menu-group/__test__/explore-menu-group.test.tsx
+++ src/components/app-switcher/explore-menu-group/__test__/explore-menu-group.test.tsx
@@ -1,15 +1,7 @@
 import { render } from '@testing-library/react'
-import { AppSwitcher } from '../../app-switcher'
 import { AppSwitcherExploreMenuGroup } from '../explore-menu-group'
 
-const sampleUrl = 'https://www.test.com'
 test('renders AppSwitcherExploreMenuGroup properly', () => {
-  const { asFragment } = render(
-    <AppSwitcherExploreMenuGroup>
-      <AppSwitcher.ReapitPMMenuItem url={sampleUrl} />
-      <AppSwitcher.ReapitSalesMenuItem url={sampleUrl} />
-      <AppSwitcher.KeyWhereMenuItem url={sampleUrl} />
-    </AppSwitcherExploreMenuGroup>,
-  )
+  const { asFragment } = render(<AppSwitcherExploreMenuGroup>Fake child</AppSwitcherExploreMenuGroup>)
   expect(asFragment()).toMatchSnapshot()
 })
--- src/components/app-switcher/your-apps-menu-group/__test__/your-apps-menu-group.test.tsx
+++ src/components/app-switcher/your-apps-menu-group/__test__/your-apps-menu-group.test.tsx
@@ -1,15 +1,7 @@
 import { render } from '@testing-library/react'
-import { AppSwitcher } from '../../app-switcher'
 import { AppSwitcherYourAppsMenuGroup } from '../your-apps-menu-group'
 
-const sampleUrl = 'https://www.test.com'
 test('renders AppSwitcherYourAppsMenuGroup properly', () => {
-  const { asFragment } = render(
-    <AppSwitcherYourAppsMenuGroup>
-      <AppSwitcher.ReapitPMMenuItem url={sampleUrl} />
-      <AppSwitcher.ReapitSalesMenuItem url={sampleUrl} />
-      <AppSwitcher.KeyWhereMenuItem url={sampleUrl} />
-    </AppSwitcherYourAppsMenuGroup>,
-  )
+  const { asFragment } = render(<AppSwitcherYourAppsMenuGroup>Fake child</AppSwitcherYourAppsMenuGroup>)
   expect(asFragment()).toMatchSnapshot()
 })
