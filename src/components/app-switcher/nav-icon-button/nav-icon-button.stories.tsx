a96a250a20641e1dfb0a1d56c390557ebbd5a34e
Kurt Doherty <kdoherty@reapit.com>
2025-06-04 09:08:13 +1000

    feat: #428 Update nav icon button

--- src/components/app-switcher/nav-icon-button/nav-icon-button.stories.tsx
+++ src/components/app-switcher/nav-icon-button/nav-icon-button.stories.tsx
@@ -1,27 +1,33 @@
+import { AppSwitcherNavIconButton } from './nav-icon-button'
+
 import type { Meta, StoryObj } from '@storybook/react'
-import AppSwitcherNavIconButton from './nav-icon-button'
 
 const meta = {
-  title: 'Components/AppSwitcher/AppSwitcherNavIconButton',
+  title: 'Components/AppSwitcher/NavIconButton',
   component: AppSwitcherNavIconButton,
+  argTypes: {
+    onClick: {
+      control: false,
+    },
+  },
 } satisfies Meta<typeof AppSwitcherNavIconButton>
 
 export default meta
 
 type Story = StoryObj<typeof meta>
 
-export const Default: Story = {
+export const Example: Story = {
   args: {
-    onClick: () => {},
+    onClick: () => void 0,
   },
   render: (args) => {
     return <AppSwitcherNavIconButton {...args} />
   },
 }
 
-export const WhenTheNavIconButtonIsSelected: Story = {
+export const Expanded: Story = {
   args: {
-    onClick: () => {},
+    onClick: () => void 0,
   },
   render: (args) => {
     return <AppSwitcherNavIconButton aria-expanded={true} {...args} />
