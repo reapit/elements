f94e741cb7ebb26b93c5a8cf63a43af5c0f0c99a
Kurt Doherty <kdoherty@reapit.com>
2025-06-04 16:33:30 +1000

    Add extra group tests

--- src/components/app-switcher/explore-menu-group/__test__/explore-menu-group.test.tsx
+++ src/components/app-switcher/explore-menu-group/__test__/explore-menu-group.test.tsx
@@ -1,7 +1,25 @@
 import { render } from '@testing-library/react'
 import { AppSwitcherExploreMenuGroup } from '../explore-menu-group'
+import { AppSwitcherMenuGroupHasAccessContext } from '../../menu-group-has-access-context'
 
-test('renders AppSwitcherExploreMenuGroup properly', () => {
+test('renders a group element', () => {
   const { asFragment } = render(<AppSwitcherExploreMenuGroup>Fake child</AppSwitcherExploreMenuGroup>)
   expect(asFragment()).toMatchSnapshot()
+})
+
+test('provides an `AppSwitcherMenuGroupHasAccessContext` value of `false`', () => {
+  let hasAccess: boolean | undefined
+
+  render(
+    <AppSwitcherExploreMenuGroup>
+      <AppSwitcherMenuGroupHasAccessContext.Consumer>
+        {(value) => {
+          hasAccess = value
+          return null
+        }}
+      </AppSwitcherMenuGroupHasAccessContext.Consumer>
+    </AppSwitcherExploreMenuGroup>,
+  )
+
+  expect(hasAccess).toBe(false)
 })
--- src/components/app-switcher/your-apps-menu-group/__test__/your-apps-menu-group.test.tsx
+++ src/components/app-switcher/your-apps-menu-group/__test__/your-apps-menu-group.test.tsx
@@ -1,7 +1,25 @@
 import { render } from '@testing-library/react'
 import { AppSwitcherYourAppsMenuGroup } from '../your-apps-menu-group'
+import { AppSwitcherMenuGroupHasAccessContext } from '../../menu-group-has-access-context'
 
 test('renders AppSwitcherYourAppsMenuGroup properly', () => {
   const { asFragment } = render(<AppSwitcherYourAppsMenuGroup>Fake child</AppSwitcherYourAppsMenuGroup>)
   expect(asFragment()).toMatchSnapshot()
+})
+
+test('provides an `AppSwitcherMenuGroupHasAccessContext` value of `true`', () => {
+  let hasAccess: boolean | undefined
+
+  render(
+    <AppSwitcherYourAppsMenuGroup>
+      <AppSwitcherMenuGroupHasAccessContext.Consumer>
+        {(value) => {
+          hasAccess = value
+          return null
+        }}
+      </AppSwitcherMenuGroupHasAccessContext.Consumer>
+    </AppSwitcherYourAppsMenuGroup>,
+  )
+
+  expect(hasAccess).toBe(true)
 })
