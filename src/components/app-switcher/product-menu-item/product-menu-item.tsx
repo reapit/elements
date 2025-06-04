58df3cbdc4041e08bba41f3e55ea03573a8560f8
Kurt Doherty <kdoherty@reapit.com>
2025-06-04 15:16:46 +1000

    feat: #428 Add new product menu item

---
+++ src/components/app-switcher/product-menu-item/product-menu-item.tsx
@@ -0,0 +1,43 @@
+import { AppSwitcher } from '../app-switcher'
+import { productConfigs } from '../config'
+import { useAppSwitcherMenuGroupHasAccessContext } from '../menu-group-has-access-context'
+
+import type { AppSwitcherMenuItemProps } from '../menu-item'
+import type { SupportedProductId } from '../config'
+import type { ReactNode } from 'react'
+
+// NOTE: We extend AppSwitcherMenuItemProps to allow overriding the appName, logo, and supplementaryInfo.
+// This is purely an escape hatch for consumers to reach for if they can't upgrade to the latest version
+// of Elements but need to make some UI updates for one or more products.
+interface AppSwitcherProductMenuItemProps extends Partial<AppSwitcherMenuItemProps> {
+  productId: SupportedProductId
+  url: string
+}
+
+/**
+ * A menu item for the App Switcher that represents a specific product. It displays the product's name, logo,
+ * and supplementary info based on the provided product ID. Ultimately, this is a thin wrapper around the
+ * low-level `AppSwitcher.MenuItem` component, but it provides a more convenient interface for consumers to
+ * interact with when rendering items based on the currently logged-in user's accessible products.
+ *
+ * The product's logo (`AppAvatar`) will automatically reflect the user's access to the product based on whether
+ * the item is rendered within the `AppSwitcher.YouAppsMenuGroup` or the `AppSwitcher.ExploreMenuGroup`.
+ */
+export function AppSwitcherProductMenuItem({
+  appName: appNameOverride,
+  avatar: logoOverride,
+  productId,
+  supplementaryInfo: supplementaryInfoOverride,
+  url,
+}: AppSwitcherProductMenuItemProps): ReactNode {
+  const hasAccess = useAppSwitcherMenuGroupHasAccessContext()
+  const { appName, supplementaryInfo } = productConfigs[productId]
+  return (
+    <AppSwitcher.MenuItem
+      appName={appNameOverride ?? appName}
+      supplementaryInfo={supplementaryInfoOverride ?? supplementaryInfo}
+      avatar={logoOverride ?? <AppSwitcher.AppAvatar hasAccess={hasAccess} productId={productId} />}
+      url={url}
+    />
+  )
+}
