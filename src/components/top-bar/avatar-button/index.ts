d8edad5209a7a0d586b6e8896caa909c21085300
Kurt Doherty <kdoherty@reapit.com>
2025-06-10 16:46:58 +1000

    chore: #483 refactor `AvatarButton` into top bar folder

--- src/components/top-bar/avatar-button/index.ts
+++ src/components/top-bar/avatar-button/index.ts
@@ -1,2 +1,2 @@
-export * from './styles'
 export * from './avatar-button'
+export * from './styles'
---
+++ src/components/top-bar/avatar-button/styles.ts
@@ -0,0 +1,20 @@
+import { styled } from '@linaria/react'
+import { ElAvatar } from '../../avatar/styles'
+
+export const ElTopBarAvatarButton = styled.button`
+  cursor: pointer;
+  background: none;
+  border: none;
+  padding: 0;
+  border-radius: var(--comp-navigation-border-radius-avatar_button);
+
+  &:focus-visible {
+    background: #fff;
+    outline: var(--border-width-double) solid var(--colour-border-focus);
+    outline-offset: 1px;
+  }
+
+  &:hover ${ElAvatar} {
+    background: var(--comp-navigation-colour-fill-avatar_button-hover);
+  }
+`
