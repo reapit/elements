39217cca73475c1e485f475db32516f47bf2c375
Kurt Doherty <kdoherty@reapit.com>
2025-06-13 19:06:44 +1000

    chore: #503 add `font` and `Text` helpers

---
+++ src/components/text/text.stories.tsx
@@ -0,0 +1,100 @@
+import { Text } from './text'
+import { fontSizes, fontWeights, textColours } from './types'
+
+import type { Meta, StoryObj } from '@storybook/react'
+
+const meta = {
+  title: 'Components/Text',
+  component: Text,
+  argTypes: {
+    as: {
+      control: 'select',
+      options: ['span', 'strong', 'em', 'mark', 'q', 's', 'time'],
+      table: {
+        type: { summary: 'union' },
+      },
+    },
+    children: {
+      control: 'text',
+      table: {
+        type: { summary: 'ReactNode' },
+      },
+    },
+    colour: {
+      control: 'select',
+      options: textColours,
+      table: {
+        type: { summary: 'union' },
+      },
+    },
+    size: {
+      control: 'select',
+      options: fontSizes,
+      table: {
+        type: { summary: 'union' },
+      },
+    },
+    weight: {
+      control: 'select',
+      options: fontWeights,
+      table: {
+        type: { summary: 'union' },
+      },
+    },
+  },
+} satisfies Meta<typeof Text>
+
+export default meta
+
+type Story = StoryObj<typeof Text>
+
+export const Example: Story = {
+  args: {
+    as: 'span',
+    children: 'A semantically meaningful portion of text',
+    colour: 'primary',
+    size: 'base',
+    weight: 'regular',
+  },
+}
+
+/**
+ * The `as` prop allows you to render the Text component as a different inline HTML element.
+ */
+export const Element: Story = {
+  args: {
+    ...Example.args,
+    as: 'mark',
+  },
+}
+
+/**
+ * The `colour` prop controls the text color. The available values are defined by the design system.
+ */
+export const Colour: Story = {
+  args: {
+    ...Example.args,
+    colour: 'action',
+  },
+}
+
+/**
+ * The `size` prop controls the font size.
+ */
+export const Size: Story = {
+  args: {
+    ...Example.args,
+    size: 'xs',
+  },
+}
+
+/**
+ * The `weight` prop controls the font weight.
+ */
+export const Weight: Story = {
+  args: {
+    ...Example.args,
+    as: 'strong',
+    weight: 'bold',
+  },
+}
---
+++ src/components/text/text.tsx
@@ -0,0 +1,69 @@
+import { ElText } from './styles'
+
+import type { FontSize, FontWeight, TextColour } from './types'
+import type { HTMLAttributes, QuoteHTMLAttributes, TimeHTMLAttributes } from 'react'
+
+interface BaseTextProps {
+  colour?: TextColour
+  size?: FontSize
+  weight?: FontWeight
+}
+
+interface TextAsAbbrProps extends BaseTextProps, HTMLAttributes<HTMLElement> {
+  as: 'abbr'
+}
+
+interface TextAsEmProps extends BaseTextProps, HTMLAttributes<HTMLElement> {
+  as: 'em'
+}
+
+interface TextAsMarkProps extends BaseTextProps, HTMLAttributes<HTMLElement> {
+  as: 'mark'
+}
+
+interface TextAsQuoteProps extends BaseTextProps, QuoteHTMLAttributes<HTMLQuoteElement> {
+  as: 'q'
+}
+
+interface TextAsStrikethroughProps extends BaseTextProps, HTMLAttributes<HTMLElement> {
+  as: 's'
+}
+
+interface TextAsSpanProps extends BaseTextProps, HTMLAttributes<HTMLSpanElement> {
+  as?: 'span'
+}
+
+interface TextAsStrongProps extends BaseTextProps, HTMLAttributes<HTMLElement> {
+  as: 'strong'
+}
+
+interface TextAsTimeProps extends BaseTextProps, TimeHTMLAttributes<HTMLTimeElement> {
+  as: 'time'
+}
+
+type TextProps =
+  | TextAsAbbrProps
+  | TextAsEmProps
+  | TextAsMarkProps
+  | TextAsQuoteProps
+  | TextAsStrikethroughProps
+  | TextAsSpanProps
+  | TextAsStrongProps
+  | TextAsTimeProps
+
+/**
+ * A simple component that can be used to display text in a particular size, weight and colour. If
+ * you're just looking to apply appropriate font styles to a styled element based on a particular
+ * combination of font size and/or weight, the [font](/docs/components-text-font--docs) helper
+ * is preferable.
+ *
+ * **Important:** Direct use of this component is strongly discouraged. It is primarily intended as an
+ * escape hatch when prototyping UI that is not yet supported by the Design System. Within the Design
+ * System, the `font` helper function exported alongside this component is the preferred way to apply
+ * font styles to styled elements. As such, it currently supports an intentionally limited set of HTML
+ * elements focused on inline text semantics. This is because we want this component to be minimally
+ * useful.
+ */
+export function Text({ as = 'span', colour = 'primary', size = 'base', weight = 'regular', ...props }: TextProps) {
+  return <ElText as={as} data-colour={colour} data-font-size={size} data-font-weight={weight} {...props} />
+}
