7c32289ec07602c7ca5d2e9c9b4d62fdb374667d
foxy <foxy@foxy.com>
2025-06-17 06:27:00 +0700

    chore: #421-variable-token-update-tooltips

--- src/components/tooltip/styles.ts
+++ src/components/tooltip/styles.ts
@@ -23,16 +23,11 @@ export const ElTooltip = styled.div`
     opacity: 0;
   }
 
+  border-radius: var(--comp-tooltip-border-radius);
+  background: var(--comp-tooltip-colour-fill);
+
   width: max-content;
-  background: var(--fill-default-darkest, #222b33);
-  color: var(--text-white);
   padding: var(--spacing-2) var(--spacing-3) var(--spacing-2) var(--spacing-3);
-  border-radius: var(--corner-default);
-  font-family: var(--font-family);
-  font-size: var(--font-size-xs);
-  font-weight: var(--font-weight-regular);
-  line-height: var(--line-height-xs);
-  letter-spacing: var(--letter-spacing-xs);
   text-align: left;
   position: absolute;
   transition: opacity 0.2s;
@@ -44,6 +39,16 @@ export const ElTooltip = styled.div`
   animation-duration: 0.2s;
   animation-fill-mode: both;
   animation-timing-function: ease-in-out;
+
+  color: var(--comp-tooltip-colour-text);
+
+  /* text-xs/Regular */
+  font-family: var(--font-xs-regular-family);
+  font-size: var(--font-xs-regular-size);
+  font-style: normal;
+  font-weight: var(--font-xs-regular-weight);
+  line-height: var(--font-xs-regular-line_height);
+  letter-spacing: var(--font-xs-regular-letter_spacing);
 
   &[data-is-visible='true'] {
     animation-name: fadeIn;
@@ -58,10 +63,5 @@ export const ElTooltip = styled.div`
 `
 
 export const ElTooltipLabel = styled.span`
-  font-family: var(--font-family);
-  font-size: var(--font-size-xs);
-  font-weight: var(--font-weight-semibold);
-  line-height: var(--line-height-xs);
-  letter-spacing: var(--letter-spacing-xs);
   text-align: left;
 `
