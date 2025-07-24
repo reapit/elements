38ea7002a7013930ac3c570231fa169e45a15b4c
Kurt Doherty <kdoherty@reapit.com>
2025-07-24 14:51:54 +1000

    chore!: update icons

--- src/icons/elipsis.tsx
+++
@@ -1,4 +0,0 @@
-import ElipsisSvg from './svgs/elipsis.svg?react'
-import { makeIcon } from './make-icon'
-
-export const ElipsisIcon = makeIcon('ElipsisIcon', ElipsisSvg)
---
+++ src/icons/more-vert.tsx
@@ -0,0 +1,4 @@
+import MoreVertSvg from './svgs/more-vert.svg?react'
+import { makeIcon } from './make-icon'
+
+export const MoreVertIcon = makeIcon('MoreVertIcon', MoreVertSvg)
