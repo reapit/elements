2df2c99259675fbb4dba65cdca3f37ff6941ffc6
Kurt Doherty <kdoherty@reapit.com>
2025-07-30 14:58:25 +1000

    feat: Add new `MenuDivider`

---
+++ src/core/menu/divider/styles.ts
@@ -0,0 +1,7 @@
+import { styled } from '@linaria/react'
+
+export const ElMenuDivider = styled.hr`
+  height: 0;
+  border: none;
+  border-bottom: var(--comp-divider-border-width) solid var(--comp-divider-colour-border-solid);
+`
