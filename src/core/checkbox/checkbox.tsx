17c69b825a6fc2a7ef276a63102cc29bb2d71ac2
Kurt Doherty <kdoherty@reapit.com>
2025-08-01 12:20:49 +1000

    fix: `Checkbox` now supports consumer-supplied refs

--- src/core/checkbox/checkbox.tsx
+++ src/core/checkbox/checkbox.tsx
@@ -1,6 +1,7 @@
-import React, { InputHTMLAttributes, useEffect, useRef } from 'react'
+import React, { forwardRef, InputHTMLAttributes, useEffect, useRef } from 'react'
 import { ElCheckboxIcon, ElCheckboxSelectedIcon, ElCheckboxIndeterminateIcon } from './checkbox.atoms'
 import { ElCheckbox, ElCheckboxInput, ElCheckboxLabelText, ElCheckboxSupplementaryInfo } from './styles'
+import mergeRefs from '#src/helpers/mergeRefs'
 
 /**
  * Interface for the Checkbox component props.
@@ -27,38 +28,33 @@ export interface CheckboxProps extends InputHTMLAttributes<HTMLInputElement> {
  * This component combines the atomic checkbox components (ElCheckbox, ElCheckboxInput, etc.)
  * into a single, reusable checkbox component.
  */
-export const Checkbox: React.FC<CheckboxProps> = ({
-  label,
-  supplementaryInfo,
-  required,
-  isIndeterminate = false,
-  className,
-  ...rest
-}) => {
-  const inputRef = useRef<HTMLInputElement>(null)
-  useEffect(() => {
-    if (inputRef.current) {
-      inputRef.current.indeterminate = !!isIndeterminate // Use prop value
-    }
-  }, [isIndeterminate]) // Run effect when isIndeterminate changes
+export const Checkbox = forwardRef<HTMLInputElement, CheckboxProps>(
+  ({ label, supplementaryInfo, required, isIndeterminate = false, className, ...rest }, ref) => {
+    const inputRef = useRef<HTMLInputElement>(null)
+    useEffect(() => {
+      if (inputRef.current) {
+        inputRef.current.indeterminate = !!isIndeterminate // Use prop value
+      }
+    }, [isIndeterminate]) // Run effect when isIndeterminate changes
 
-  return (
-    <ElCheckbox className={className}>
-      {/* The actual checkbox input element. Forwards the ref and other input props. */}
-      <ElCheckboxInput type="checkbox" ref={inputRef} {...rest} aria-hidden />
-      {/* Icons for different checkbox states (unchecked, checked, indeterminate). */}
-      {/* aria-hidden is used to prevent screen readers from announcing these decorative icons. */}
-      <ElCheckboxIcon aria-hidden />
-      <ElCheckboxSelectedIcon aria-hidden />
-      {isIndeterminate && <ElCheckboxIndeterminateIcon aria-hidden />}
-      {/* Label text for the checkbox. Styled using data attributes for variant and size. */}
-      <ElCheckboxLabelText variant="strong" size="medium" isRequired={required}>
-        {label}
-      </ElCheckboxLabelText>
-      {/* Supplementary information displayed below the label. Styled using data attributes. */}
-      <ElCheckboxSupplementaryInfo variant="soft" size="small">
-        {supplementaryInfo}
-      </ElCheckboxSupplementaryInfo>
-    </ElCheckbox>
-  )
-}
+    return (
+      <ElCheckbox className={className}>
+        {/* The actual checkbox input element. Forwards the ref and other input props. */}
+        <ElCheckboxInput type="checkbox" ref={mergeRefs(inputRef, ref)} {...rest} aria-hidden />
+        {/* Icons for different checkbox states (unchecked, checked, indeterminate). */}
+        {/* aria-hidden is used to prevent screen readers from announcing these decorative icons. */}
+        <ElCheckboxIcon aria-hidden />
+        <ElCheckboxSelectedIcon aria-hidden />
+        {isIndeterminate && <ElCheckboxIndeterminateIcon aria-hidden />}
+        {/* Label text for the checkbox. Styled using data attributes for variant and size. */}
+        <ElCheckboxLabelText variant="strong" size="medium" isRequired={required}>
+          {label}
+        </ElCheckboxLabelText>
+        {/* Supplementary information displayed below the label. Styled using data attributes. */}
+        <ElCheckboxSupplementaryInfo variant="soft" size="small">
+          {supplementaryInfo}
+        </ElCheckboxSupplementaryInfo>
+      </ElCheckbox>
+    )
+  },
+)
