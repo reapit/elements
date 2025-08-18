reapit/elements

commit a2e51f165f5d6ae28a032069ab59d1b61dfef24e

chore: use new breakpoints in core components (#687)

src/core/top-bar/styles.ts
@@ -1,29 +1,35 @@
-import { styled } from '@linaria/react'
-import { isTablet, isDesktop, isWideScreen } from '../../styles/deprecated-media'
 import { css } from '@linaria/core'
+import { isWidthAtOrAbove } from '#src/utils/breakpoints'
+import { styled } from '@linaria/react'
+import { TOP_BAR_CONTAINER_NAME } from './constants'
 
 export const ElTopBar = styled.header`
+  height: var(--size-14);
+  width: 100%;
+
+  container-name: ${TOP_BAR_CONTAINER_NAME};
+  container-type: inline-size;
+
+  border-bottom: var(--comp-navigation-border-width-top_bar) solid var(--comp-navigation-colour-border-top_bar);
+  background: var(--comp-navigation-colour-fill-top_bar);
+`
+
+export const ElTopBarContentContainer = styled.div`
   display: grid;
   align-items: center;
   grid-template-areas: 'app-switcher logo main-nav search secondary-nav mobile-nav profile';
   grid-template-columns: min-content min-content 1fr auto auto auto auto;
-  height: var(--size-14);
-  width: 100%;
-
-  container-name: top-bar;
-  container-type: inline-size;
 
   padding-block: var(--spacing-2);
   padding-inline: var(--spacing-4) var(--spacing-2);
-  ${isTablet} {
+
+  @container ${TOP_BAR_CONTAINER_NAME} ${isWidthAtOrAbove('SM')} {
     padding-inline: var(--spacing-4);
   }
-  ${isWideScreen} {
+
+  @container ${TOP_BAR_CONTAINER_NAME} ${isWidthAtOrAbove('LG')} {
     padding-inline: var(--spacing-5);
   }
-
-  border-bottom: var(--comp-navigation-border-width-top_bar) solid var(--comp-navigation-colour-border-top_bar);
-  background: var(--comp-navigation-colour-fill-top_bar);
 `
 
 export const ElTopBarAppSwitcherContainer = styled.div`
@@ -31,24 +37,13 @@ export const ElTopBarAppSwitcherContainer = styled.div`
 
   display: none;
   padding-inline-end: var(--spacing-4);
-  @supports not (container: inline-size) {
-    ${isTablet} {
-      display: block;
-    }
-    ${isWideScreen} {
-      padding-inline-end: var(--spacing-5);
-    }
+
+  @container ${TOP_BAR_CONTAINER_NAME} ${isWidthAtOrAbove('SM')} {
+    display: block;
   }
 
-  @supports (container: inline-size) {
-    /* isTablet equivalent inline size; i.e. 768px - 2 * var(--spacing-4) */
-    @container top-bar (width >= 736px) {
-      display: block;
-    }
-    /* isWideScreen equivalent inline size; i.e. 1440px - 2 * var(--spacing-5) */
-    @container top-bar (width >= 1400px) {
-      padding-inline-end: var(--spacing-5);
-    }
+  @container ${TOP_BAR_CONTAINER_NAME} ${isWidthAtOrAbove('LG')} {
+    padding-inline-end: var(--spacing-5);
   }
 `
 
@@ -67,17 +62,8 @@ export const ElTopBarAvatarContainer = styled.div`
 
   display: none;
 
-  @supports not (container: inline-size) {
-    ${isDesktop} {
-      display: flex;
-    }
-  }
-
-  @supports (container: inline-size) {
-    /* isDesktop equivalent inline size; i.e. 1024px - 2 * var(--spacing-4) */
-    @container top-bar (width >= 992px) {
-      display: flex;
-    }
+  @container ${TOP_BAR_CONTAINER_NAME} ${isWidthAtOrAbove('MD')} {
+    display: flex;
   }
 `
 
@@ -92,17 +78,8 @@ export const ElTopBarMainNavContainer = styled.div`
 
   display: none;
 
-  @supports not (container: inline-size) {
-    ${isWideScreen} {
-      display: block;
-    }
-  }
-
-  @supports (container: inline-size) {
-    /* isWideScreen equivalent inline size; i.e. 1440px - 2 * var(--spacing-5) */
-    @container top-bar (width >= 1400px) {
-      display: block;
-    }
+  @container ${TOP_BAR_CONTAINER_NAME} ${isWidthAtOrAbove('LG')} {
+    display: block;
   }
 `
 
@@ -112,17 +89,8 @@ export const ElTopBarSecondaryNavContainer = styled.div`
 
   display: none;
 
-  @supports not (container: inline-size) {
-    ${isWideScreen} {
-      display: block;
-    }
-  }
-
-  @supports (container: inline-size) {
-    /* isWideScreen equivalent inline size; i.e. 1440px - 2 * var(--spacing-5) */
-    @container top-bar (width >= 1400px) {
-      display: block;
-    }
+  @container ${TOP_BAR_CONTAINER_NAME} ${isWidthAtOrAbove('SM')} {
+    display: block;
   }
 `
 
@@ -133,21 +101,10 @@ export const ElTopBarSearchContainer = styled.div`
   padding-block: var(--spacing-none);
   padding-inline-end: var(--spacing-2);
 
-  @supports not (container: inline-size) {
-    ${isTablet} {
-      width: 216px;
-      padding-block: var(--spacing-1);
-      padding-inline-end: var(--spacing-4);
-    }
-  }
-
-  @supports (container: inline-size) {
-    /* isTablet equivalent inline size; i.e. 768px - 2 * var(--spacing-4) */
-    @container top-bar (width >= 736px) {
-      width: 216px;
-      padding-block: var(--spacing-1);
-      padding-inline-end: var(--spacing-4);
-    }
+  @container ${TOP_BAR_CONTAINER_NAME} ${isWidthAtOrAbove('SM')} {
+    width: 216px;
+    padding-block: var(--spacing-1);
+    padding-inline-end: var(--spacing-4);
   }
 `
 
@@ -157,24 +114,12 @@ export const ElTopBarMenuContainer = styled.div`
   display: block;
   padding-inline-end: 0;
 
-  @supports not (container: inline-size) {
-    ${isDesktop} {
-      padding-inline-end: var(--spacing-2);
-    }
-    ${isWideScreen} {
-      display: none;
-    }
+  @container ${TOP_BAR_CONTAINER_NAME} ${isWidthAtOrAbove('MD')} {
+    padding-inline-end: var(--spacing-2);
   }
 
-  @supports (container: inline-size) {
-    /* isDesktop equivalent inline size; i.e. 1024px - 2 * var(--spacing-4) */
-    @container top-bar (width >= 992px) {
-      padding-inline-end: var(--spacing-2);
-    }
-    /* isWideScreen equivalent inline size; i.e. 1440px - 2 * var(--spacing-5) */
-    @container top-bar (width >= 1400px) {
-      display: none;
-    }
+  @container ${TOP_BAR_CONTAINER_NAME} ${isWidthAtOrAbove('LG')} {
+    display: none;
   }
 `
 

src/core/bottom-bar/item/styles.ts
@@ -36,13 +36,12 @@ export const ElBottomBarItemIcon = styled.span`
   position: relative;
 
   color: var(--comp-navigation-colour-icon-bottom_bar-default);
-  font-size: var(--icon_size-l);
   width: var(--icon_size-l);
   height: var(--icon_size-l);
 
   ${ElDeprecatedIcon} {
     color: inherit;
-    font-size: inherit;
+    font-size: var(--icon_size-l);
     width: inherit;
     height: inherit;
   }

src/core/bottom-bar/styles.ts
@@ -1,4 +1,4 @@
-import { isTablet } from '#src/styles/deprecated-media'
+import { isWidthAtOrAbove, isWidthBelow } from '#src/utils/breakpoints'
 import { styled } from '@linaria/react'
 
 export const ElBottomBarContainer = styled.div`
@@ -10,16 +10,20 @@ export const ElBottomBarContainer = styled.div`
   container-type: inline-size;
 
   display: block;
-  @supports not (container-type: inline-size) {
-    ${isTablet} {
-      display: none;
-    }
+
+  @media screen and ${isWidthAtOrAbove('SM')} {
+    display: none;
   }
-  @supports (container-type: inline-size) {
-    /* NOTE: This is equivalent to the SM breakpoint */
-    @container (width >= 768px) {
-      display: none;
-    }
+
+  @container ${isWidthAtOrAbove('SM')} {
+    display: none;
+  }
+
+  /* NOTE: This container query will override the default media query behaviour above if there's
+   * an ancestor is a container. If there's no ancestral container, the media query will behave
+   * as defined above. */
+  @container ${isWidthBelow('SM')} {
+    display: block;
   }
 `
 

src/core/breadcrumbs/styles.ts
@@ -1,5 +1,5 @@
 import { styled } from '@linaria/react'
-import { isMobile } from '#src/styles/deprecated-media'
+import { isWidthAtOrAbove, isWidthBelow } from '#src/utils/breakpoints'
 
 interface ElBreadcrumbsProps {
   'data-overflow'?: 'scroll' | 'truncate'
@@ -8,10 +8,25 @@ interface ElBreadcrumbsProps {
 export const ElBreadcrumbs = styled.nav<ElBreadcrumbsProps>`
   width: 100%;
 
-  ${isMobile} {
+  /* NOTE: Media and container queries come before data-overflow attribute styles to allow
+   * the latter to take precedence */
+  @media screen and ${isWidthBelow('SM')} {
     /* NOTE: This is generally considered bad practice */
     scrollbar-width: none;
     overflow-x: auto;
+  }
+
+  @container ${isWidthBelow('SM')} {
+    scrollbar-width: none;
+    overflow-x: auto;
+  }
+
+  /* NOTE: This container query will override the default media query behaviour above if there's
+   * an ancestor is a container. If there's no ancestral container, the media query will behave
+   * as defined above. */
+  @container ${isWidthAtOrAbove('SM')} {
+    scrollbar-width: initial;
+    overflow-x: initial;
   }
 
   &[data-overflow='scroll'] {
@@ -34,8 +49,21 @@ export const ElBreadcrumbsList = styled.ul`
 
   width: 100%;
 
-  ${isMobile} {
+  /* NOTE: Media and container queries come before data-overflow attribute styles to allow
+   * the latter to take precedence */
+  @media screen and ${isWidthBelow('SM')} {
     width: max-content;
+  }
+
+  @container ${isWidthBelow('SM')} {
+    width: max-content;
+  }
+
+  /* NOTE: This container query will override the default media query behaviour above if there's
+   * an ancestor is a container. If there's no ancestral container, the media query will behave
+   * as defined above. */
+  @container ${isWidthAtOrAbove('SM')} {
+    width: 100%;
   }
 
   [data-overflow='truncate'] > & {

src/core/page-header/leading-element/styles.ts
@@ -1,4 +1,4 @@
-import { isTablet } from '#src/styles/deprecated-media'
+import { isWidthAtOrAbove, isWidthBelow } from '#src/utils/breakpoints'
 import { styled } from '@linaria/react'
 
 interface PageHeaderLeadingElementProps {
@@ -19,24 +19,25 @@ export const ElPageHeaderLeadingElement = styled.div<PageHeaderLeadingElementPro
     height: var(--size-6);
     width: var(--size-6);
 
-    ${isTablet} {
+    @media screen and ${isWidthAtOrAbove('SM')} {
       padding-block-start: var(--spacing-1);
       height: var(--size-10);
       width: var(--size-10);
     }
 
-    @supports (container-type: inline-size) {
-      @container (width < 768px) {
-        padding-block-start: var(--spacing-2);
-        height: var(--size-6);
-        width: var(--size-6);
-      }
+    @container ${isWidthAtOrAbove('SM')} {
+      padding-block-start: var(--spacing-1);
+      height: var(--size-10);
+      width: var(--size-10);
+    }
 
-      @container (width >= 768px) {
-        padding-block-start: var(--spacing-1);
-        height: var(--size-10);
-        width: var(--size-10);
-      }
+    /* NOTE: This container query will override the default media query behaviour above if there's
+     * an ancestor is a container. If there's no ancestral container, the media query will behave
+     * as defined above. */
+    @container ${isWidthBelow('SM')} {
+      padding-block-start: var(--spacing-2);
+      height: var(--size-6);
+      width: var(--size-6);
     }
   }
 
@@ -47,18 +48,19 @@ export const ElPageHeaderLeadingElement = styled.div<PageHeaderLeadingElementPro
     padding-block-start: var(--spacing-1);
     width: var(--size-16);
 
-    ${isTablet} {
+    @media screen and ${isWidthAtOrAbove('SM')} {
       width: var(--size-24);
     }
 
-    @supports (container-type: inline-size) {
-      @container (width < 768px) {
-        width: var(--size-16);
-      }
+    @container ${isWidthAtOrAbove('SM')} {
+      width: var(--size-24);
+    }
 
-      @container (width >= 768px) {
-        width: var(--size-24);
-      }
+    /* NOTE: This container query will override the default media query behaviour above if there's
+     * an ancestor is a container. If there's no ancestral container, the media query will behave
+     * as defined above. */
+    @container ${isWidthBelow('SM')} {
+      width: var(--size-16);
     }
   }
 `

src/core/page-header/styles.ts
@@ -1,4 +1,4 @@
-import { isDesktop, isTablet } from '#src/styles/deprecated-media'
+import { isWidthAtOrAbove, isWidthBelow } from '#src/utils/breakpoints'
 import { styled } from '@linaria/react'
 
 import type { CSSProperties } from 'react'
@@ -24,12 +24,12 @@ export const ElPageHeader = styled.header<PageHeaderProps>`
 
   /* NOTE: Media queries are use by default, but if the browser supports container queries, they will override the
    * media query styles defined here. */
-  ${isTablet} {
+  @media screen and ${isWidthAtOrAbove('SM')} {
     padding-block: var(--spacing-8);
     padding-inline: var(--spacing-8);
   }
 
-  ${isDesktop} {
+  @media screen and ${isWidthAtOrAbove('MD')} {
     padding-block: var(--spacing-10);
     padding-inline: var(--spacing-10);
   }
@@ -37,21 +37,19 @@ export const ElPageHeader = styled.header<PageHeaderProps>`
   /* NOTE: These container query styles come _after_ the media query styles, so they will override them (if the browser
    * supports container queries). These container query styles are also dependent on a parent element being a size or
    * inline-size container. */
-  @supports (container-type: inline-size) {
-    @container (width < 768px) {
-      padding-block: var(--spacing-3);
-      padding-inline: var(--spacing-5) var(--spacing-3);
-    }
+  @container ${isWidthBelow('SM')} {
+    padding-block: var(--spacing-3);
+    padding-inline: var(--spacing-5) var(--spacing-3);
+  }
 
-    @container (width >= 768px) {
-      padding-block: var(--spacing-8);
-      padding-inline: var(--spacing-8);
-    }
+  @container ${isWidthBelow('MD')} {
+    padding-block: var(--spacing-8);
+    padding-inline: var(--spacing-8);
+  }
 
-    @container (width >= 1024px) {
-      padding-block: var(--spacing-8);
-      padding-inline: var(--spacing-10);
-    }
+  @container ${isWidthBelow('MD')} {
+    padding-block: var(--spacing-8);
+    padding-inline: var(--spacing-10);
   }
 `
 
@@ -61,21 +59,19 @@ export const ElPageHeaderBreadcrumbsContainer = styled.div`
 
   /* NOTE: Media queries are use by default, but if the browser supports container queries, they will override the
    * media query styles defined here. */
-  ${isTablet} {
+  @media screen and ${isWidthAtOrAbove('SM')} {
+    padding-block-end: var(--spacing-4);
+  }
+
+  @container ${isWidthAtOrAbove('SM')} {
     padding-block-end: var(--spacing-4);
   }
 
   /* NOTE: These container query styles come _after_ the media query styles, so they will override them (if the browser
-  * supports container queries). These container query styles are also dependent on a parent element being a size or
-  * inline-size container. */
-  @supports (container-type: inline-size) {
-    @container (width < 768px) {
-      padding-block-end: var(--spacing-2);
-    }
-
-    @container (width >= 768px) {
-      padding-block-end: var(--spacing-4);
-    }
+   * supports container queries). These container query styles are also dependent on a parent element being a size or
+   * inline-size container. */
+  @container ${isWidthBelow('SM')} {
+    padding-block-end: var(--spacing-2);
   }
 `
 
@@ -85,21 +81,19 @@ export const ElPageHeaderLeadingElementContainer = styled.div`
 
   /* NOTE: Media queries are use by default, but if the browser supports container queries, they will override the
    * media query styles defined here. */
-  ${isTablet} {
+  @media screen and ${isWidthAtOrAbove('SM')} {
+    padding-inline-end: var(--spacing-4);
+  }
+
+  @container ${isWidthAtOrAbove('SM')} {
     padding-inline-end: var(--spacing-4);
   }
 
   /* NOTE: These container query styles come _after_ the media query styles, so they will override them (if the browser
-  * supports container queries). These container query styles are also dependent on a parent element being a size or
-  * inline-size container. */
-  @supports (container-type: inline-size) {
-    @container (width < 768px) {
-      padding-inline-end: var(--spacing-3);
-    }
-
-    @container (width >= 768px) {
-      padding-inline-end: var(--spacing-4);
-    }
+   * supports container queries). These container query styles are also dependent on a parent element being a size or
+   * inline-size container. */
+  @container ${isWidthBelow('SM')} {
+    padding-inline-end: var(--spacing-3);
   }
 `
 

src/core/top-bar/nav-search/styles.ts
@@ -1,4 +1,3 @@
-import { isTablet } from '#src/styles/deprecated-media'
 import { styled } from '@linaria/react'
 
 export const ElTopBarNavSearch = styled.div`
@@ -9,29 +8,15 @@ export const ElTopBarNavSearch = styled.div`
 export const ElTopBarNavSearchButtonContainer = styled.div`
   display: none;
 
-  @supports not (container-type: inline-size) {
-    ${isTablet} {
-      display: block;
-    }
-  }
-  @supports (container-type: inline-size) {
-    @container nav-search (width >= 150px) {
-      display: block;
-    }
+  @container nav-search (width >= 150px) {
+    display: block;
   }
 `
 
 export const ElTopBarNavSearchIconItemContainer = styled.div`
   display: block;
 
-  @supports not (container-type: inline-size) {
-    ${isTablet} {
-      display: none;
-    }
-  }
-  @supports (container-type: inline-size) {
-    @container nav-search (width >= 150px) {
-      display: none;
-    }
+  @container nav-search (width >= 150px) {
+    display: none;
   }
 `

src/core/bottom-bar/bottom-bar.stories.tsx
@@ -38,11 +38,13 @@ const meta = {
       <div
         ref={ref}
         style={{
-          overflow: 'auto',
-          height: '300px',
-          width: '100%',
           boxSizing: 'border-box',
           border: '1px solid #FA00FF',
+          containerType: 'inline-size',
+          height: '300px',
+          marginInline: 'auto',
+          overflow: 'auto',
+          width: '600px',
         }}
       >
         <Pattern height="100vh" />

commit 5af053de8fcad48d505944c8de97522c3f879f2e

fix: `TopBar` secondary nav regression introduced by #687

src/core/top-bar/styles.ts
@@ -89,7 +89,7 @@ export const ElTopBarSecondaryNavContainer = styled.div`
 
   display: none;
 
-  @container ${TOP_BAR_CONTAINER_NAME} ${isWidthAtOrAbove('SM')} {
+  @container ${TOP_BAR_CONTAINER_NAME} ${isWidthAtOrAbove('LG')} {
     display: block;
   }
 `