e3a26ea864b6244d9e21a2b795f6d07b7b56beed
Kurt Doherty <kdoherty@reapit.com>
2025-08-29 09:16:41 +1000

    feat: Add new `TableHeaderRow` (#727)

---
+++ src/core/table/header-row/styles.ts
@@ -0,0 +1,27 @@
+import { css } from '@linaria/core'
+
+// NOTE: This is a plain class so that we have an exportable class name
+// available for consumers that want table row styling on an element not
+// supported by the TableHeaderRow component.
+export const elTableHeaderRow = css`
+  /* Relative positioning is critical for the row's primary action to work correctly.
+   * The TableRowPrimaryAction component relies on this relative positioning to position
+   * its ::after pseudo-element over the entire row so that clicks on the row are captured
+   * by the action rather than the row. */
+  position: relative;
+
+  display: grid;
+  grid-column: 1 / -1;
+  grid-template-columns: subgrid;
+  grid-template-rows: auto;
+  align-items: center;
+  justify-content: inherit;
+  width: 100%;
+
+  background: var(--colour-fill-white);
+  border-block-end: var(--border-width-default) solid var(--colour-border-light_default);
+  padding: 0;
+
+  min-height: var(--size-10);
+  max-height: var(--size-12);
+`
