6e70913bbf90192ed45d4f0db8278b46f8318e13
Kurt Doherty <kdoherty@reapit.com>
2025-09-02 15:15:37 +1000

    chore: remove `RadioDisabledIcon`

--- src/icons/radio-disabled.tsx
+++
@@ -1,4 +0,0 @@
-import RadioDisabledSvg from './svgs/radio-disabled.svg?react'
-import { makeIcon } from './make-icon'
-
-export const RadioDisabledIcon = makeIcon('RadioDisabledIcon', RadioDisabledSvg)
