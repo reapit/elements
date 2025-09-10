536501ef590f116239024e5b866a25ef1465a4a5
Kurt Doherty <kdoherty@reapit.com>
2025-09-10 11:32:08 +1000

    feat: add `readOnly` support to `ChipSelect` (#763)

--- src/core/chip-select/chip/chip.tsx
+++ src/core/chip-select/chip/chip.tsx
@@ -7,18 +7,21 @@ import {
 import { forwardRef, useCallback } from 'react'
 import { maybeDeselectOtherOptions } from './maybe-deselect-other-options'
 
-import type { ChangeEventHandler, InputHTMLAttributes, ReactNode } from 'react'
+import type { ChangeEventHandler, MouseEventHandler, InputHTMLAttributes, ReactNode } from 'react'
 
 // We omit a few attributes from the base input element:
+// - onClick, because we want the onClick to be handled by the label, not the input
 // - size, because we use this for our own sizing
 // - type, because chip select options are always checkboxes
-type AttributesToOmit = 'size' | 'type'
+type AttributesToOmit = 'onClick' | 'size' | 'type'
 
 export interface ChipSelectChipProps extends Omit<InputHTMLAttributes<HTMLInputElement>, AttributesToOmit> {
   /** The accessible name of the chip. Should be considered mandatory when no visual label is provided. */
   'aria-label'?: string
   /** The visual label of the chip. */
   children?: ReactNode
+  /** Whether the chip is disabled. Disabled chips, even if checked, will not be submitted. */
+  disabled?: boolean
   /**
    * The icon of the chip. All chips in the same chip select should either have an icon or not.
    * If there is no visual label provided via `children`, the icon should be considered mandatory.
@@ -35,8 +38,12 @@ export interface ChipSelectChipProps extends Omit<InputHTMLAttributes<HTMLInputE
   name?: string
   /** Callback called when the chip's checked state changes. */
   onChange?: ChangeEventHandler<HTMLInputElement>
+  /** Callback called when the chip is clicked. */
+  onClick?: MouseEventHandler<HTMLLabelElement>
   /** Whether the label of the chip should be truncated if it is too long */
   overflow?: 'truncate'
+  /** Whether the chip is read-only. */
+  readOnly?: boolean
   /** The size of the chip. All chips in the same chip select should have the same size. */
   size: 'small' | 'medium' | 'large'
   /** The value of the form control. */
@@ -49,11 +56,29 @@ export interface ChipSelectChipProps extends Omit<InputHTMLAttributes<HTMLInputE
  */
 export const ChipSelectChip = forwardRef<HTMLInputElement, ChipSelectChipProps>(
   (
-    { 'aria-label': ariaLabel, children, icon, isExclusive = false, maxWidth, onChange, overflow, size, ...rest },
+    {
+      'aria-label': ariaLabel,
+      children,
+      icon,
+      isExclusive = false,
+      maxWidth,
+      onChange,
+      onClick,
+      overflow,
+      readOnly,
+      size,
+      ...rest
+    },
     ref,
   ) => {
     const handleChange = useCallback<ChangeEventHandler<HTMLInputElement>>(
       (event) => {
+        // If the chip is read-only, do nothing.
+        if (readOnly) {
+          event.preventDefault()
+          return
+        }
+
         // We only need to deselect other options if the one whose checked state is changing
         // has been checked. If it has been unchecked, there's nothing to do.
         if (event.currentTarget.checked) {
@@ -61,13 +86,27 @@ export const ChipSelectChip = forwardRef<HTMLInputElement, ChipSelectChipProps>(
         }
         onChange?.(event)
       },
-      [onChange],
+      [onChange, readOnly],
+    )
+
+    const handleClick = useCallback<MouseEventHandler<HTMLLabelElement>>(
+      (event) => {
+        // Since the read-only attribute has no effect on checkbox inputs, we need to manually prevent
+        // click events on chips that have been marked as read-only in order to prevent the preventDefault
+        // behaviour of changing their checked state.
+        if (readOnly) {
+          event.preventDefault()
+        }
+        onClick?.(event)
+      },
+      [onClick, readOnly],
     )
 
     return (
       <ElChipSelectChip
         aria-label={ariaLabel}
         data-size={size}
+        onClick={handleClick}
         style={{ maxWidth: maxWidth ? `var(${maxWidth})` : undefined }}
       >
         <ElChipSelectChipInput
@@ -75,6 +114,7 @@ export const ChipSelectChip = forwardRef<HTMLInputElement, ChipSelectChipProps>(
           // NOTE: this attribute is tightly coupled to the `isExclusiveOption` helper.
           data-exclusive={isExclusive}
           onChange={handleChange}
+          readOnly={readOnly}
           ref={ref}
           type="checkbox"
         />
