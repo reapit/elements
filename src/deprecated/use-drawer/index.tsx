d9ea269f03dc6dfd16588fdc7633f5573791afc1
Kurt Doherty <kdoherty@reapit.com>
2025-09-11 17:06:20 +1000

    fix: reference to non-existent `DrawerProps` (#775)

--- src/deprecated/use-drawer/index.tsx
+++ src/deprecated/use-drawer/index.tsx
@@ -1,10 +1,10 @@
 import { FC, useMemo, useState } from 'react'
-import { DeprecatedDrawer as Drawer, DrawerProps } from '../drawer'
+import { DeprecatedDrawer as Drawer, DeprecatedDrawerProps } from '../drawer'
 import { Portal } from '../use-portal'
 
 /** @deprecated */
 export type UseDrawer = [
-  Drawer: FC<Partial<DrawerProps>>,
+  Drawer: FC<Partial<DeprecatedDrawerProps>>,
   openDrawer: () => void,
   closeDrawer: () => void,
   drawerIsOpen: boolean,
@@ -18,7 +18,7 @@ export const useDrawer = (id?: string): UseDrawer => {
   const closeDrawer = () => setDrawerIsOpen(false)
   const openDrawer = () => setDrawerIsOpen(true)
 
-  const DrawerComponent: FC<Partial<DrawerProps>> = ({
+  const DrawerComponent: FC<Partial<DeprecatedDrawerProps>> = ({
     children,
     isOpen = drawerIsOpen,
     onDrawerClose = closeDrawer,
