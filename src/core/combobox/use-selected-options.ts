84521865471190a6c0c743675bde2f3a9381718b
Kurt Doherty <kdoherty@reapit.com>
2025-11-25 10:05:05 +1000

    chore: clarify `useComboboxSelectedOptions` usage (#925)

--- src/core/combobox/use-selected-options.ts
+++ src/core/combobox/use-selected-options.ts
@@ -12,6 +12,9 @@ export interface ComboboxSelectedOption {
 /**
  * Tracks the currently selected options in a combobox listbox and returns them as an array.
  *
+ * **Do not use when the listbox options change dynamically (such as when a search input is
+ * used to filter the displayed options dynamically).**
+ *
  * This hook observes selection changes in the listbox element identified by `listboxId` and
  * maintains state with the label and value of each selected option.
  *
