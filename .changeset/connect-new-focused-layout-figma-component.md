d9a550d248019d2b25ab9ead4c1d39ad60cdaac8
Kurt Doherty <kdoherty@reapit.com>
2026-03-27 12:15:19 +1000

    chore: DS-126 Connect new FocusedLayout Figma component (#1241)
    ## Summary
    
    - Connect `FocusedLayout` to the new Figma component node, covering XS,
    SM, and MD-2XL breakpoint variants
    - Retain the old Figma node as a deprecated mapping to preserve existing
    Code Connect coverage
    - Add Code Connect for `FocusedLayout.ProductLogo`
    - Wire the `logo` prop via `figma.children` across all breakpoint
    mappings
    
    ## Ticket
    
    [DS-126](https://reapit-global.atlassian.net/browse/DS-126)
    
    [DS-126]:
    https://reapit-global.atlassian.net/browse/DS-126?atlOrigin=eyJpIjoiNWRkNTljNzYxNjVmNDY3MDlhMDU5Y2ZhYzA5YTRkZjUiLCJwIjoiZ2l0aHViLWNvbS1KU1cifQ

---
+++ .changeset/connect-new-focused-layout-figma-component.md
@@ -0,0 +1,5 @@
+---
+'@reapit/elements': patch
+---
+
+Internal: Update `FocusedLayout` Figma Code Connect to target the new Figma component node, retaining the deprecated node as a fallback connection. Add Code Connect for `FocusedLayout.ProductLogo` and wire the `logo` prop via `figma.children` across all breakpoint mappings
--- src/core/focused-layout/focused-layout.figma.tsx
+++ src/core/focused-layout/focused-layout.figma.tsx
@@ -9,17 +9,20 @@ figma.connect(FocusedLayout, '<FOCUSED_LAYOUT_URL>', {
       Light: 'light',
       Dark: 'dark',
     }),
+    children: figma.slot('Content slot'),
     closeButton: figma.enum('Type', {
       Simple: undefined,
       MultiStep: figma.instance('Button'),
     }),
     pageTitle: figma.string('✏️ Page title'),
-    logo: figma.instance('Logo'),
+    logo: figma.children('Product logo'),
   },
   example: (props) => (
     <FocusedLayout background={props.background}>
-      <FocusedLayout.TopBar title={props.pageTitle}>{props.closeButton}</FocusedLayout.TopBar>
-      <FocusedLayout.Content>{/* TODO: add content */}</FocusedLayout.Content>
+      <FocusedLayout.TopBar logo={props.logo} title={props.pageTitle}>
+        {props.closeButton}
+      </FocusedLayout.TopBar>
+      <FocusedLayout.Content>{props.children}</FocusedLayout.Content>
       <FocusedLayout.BottomBar>{props.actions}</FocusedLayout.BottomBar>
     </FocusedLayout>
   ),
@@ -28,22 +31,25 @@ figma.connect(FocusedLayout, '<FOCUSED_LAYOUT_URL>', {
 figma.connect(FocusedLayout, '<FOCUSED_LAYOUT_URL>', {
   variant: { Breakpoints: 'SM' },
   props: {
-    actions: figma.instance('Button group'),
+    actions: figma.children('Button group'),
     background: figma.enum('🎨 Style', {
       Light: 'light',
       Dark: 'dark',
     }),
+    children: figma.slot('Content slot'),
     closeButton: figma.enum('Type', {
       Simple: undefined,
       MultiStep: figma.instance('Button'),
     }),
     pageTitle: figma.string('✏️ Page title'),
-    logo: figma.instance('Logo'),
+    logo: figma.children('Product logo'),
   },
   example: (props) => (
     <FocusedLayout background={props.background}>
-      <FocusedLayout.TopBar title={props.pageTitle}>{props.closeButton}</FocusedLayout.TopBar>
-      <FocusedLayout.Content>{/* TODO: add content */}</FocusedLayout.Content>
+      <FocusedLayout.TopBar logo={props.logo} title={props.pageTitle}>
+        {props.closeButton}
+      </FocusedLayout.TopBar>
+      <FocusedLayout.Content>{props.children}</FocusedLayout.Content>
       <FocusedLayout.BottomBar>{props.actions}</FocusedLayout.BottomBar>
     </FocusedLayout>
   ),
@@ -52,7 +58,33 @@ figma.connect(FocusedLayout, '<FOCUSED_LAYOUT_URL>', {
 figma.connect(FocusedLayout, '<FOCUSED_LAYOUT_URL>', {
   variant: { Breakpoints: 'MD-2XL' },
   props: {
-    actions: figma.instance('Button group'),
+    actions: figma.children('Button group'),
+    background: figma.enum('🎨 Style', {
+      Light: 'light',
+      Dark: 'dark',
+    }),
+    children: figma.slot('Content slot'),
+    pageTitle: figma.string('✏️ Page title'),
+    logo: figma.children('Product logo'),
+  },
+  example: (props) => (
+    <FocusedLayout background={props.background}>
+      <FocusedLayout.TopBar logo={props.logo} title={props.pageTitle}>
+        {props.actions}
+      </FocusedLayout.TopBar>
+      <FocusedLayout.Content>{props.children}</FocusedLayout.Content>
+    </FocusedLayout>
+  ),
+})
+
+//
+// Deprecated Figma component support
+//
+
+figma.connect(FocusedLayout, '<FOCUSED_LAYOUT_URL_DEPRECATED>', {
+  variant: { Breakpoints: 'XS' },
+  props: {
+    actions: figma.children('Button group'),
     background: figma.enum('🎨 Style', {
       Light: 'light',
       Dark: 'dark',
@@ -62,11 +94,61 @@ figma.connect(FocusedLayout, '<FOCUSED_LAYOUT_URL>', {
       MultiStep: figma.instance('Button'),
     }),
     pageTitle: figma.string('✏️ Page title'),
-    logo: figma.instance('Logo'),
+    logo: figma.children('Product logo'),
   },
   example: (props) => (
     <FocusedLayout background={props.background}>
-      <FocusedLayout.TopBar title={props.pageTitle}>{props.actions}</FocusedLayout.TopBar>
+      <FocusedLayout.TopBar logo={props.logo} title={props.pageTitle}>
+        {props.closeButton}
+      </FocusedLayout.TopBar>
+      <FocusedLayout.Content>{/* TODO: add content */}</FocusedLayout.Content>
+      <FocusedLayout.BottomBar>{props.actions}</FocusedLayout.BottomBar>
+    </FocusedLayout>
+  ),
+})
+
+figma.connect(FocusedLayout, '<FOCUSED_LAYOUT_URL_DEPRECATED>', {
+  variant: { Breakpoints: 'SM' },
+  props: {
+    actions: figma.children('Button group'),
+    background: figma.enum('🎨 Style', {
+      Light: 'light',
+      Dark: 'dark',
+    }),
+    closeButton: figma.enum('Type', {
+      Simple: undefined,
+      MultiStep: figma.instance('Button'),
+    }),
+    pageTitle: figma.string('✏️ Page title'),
+    logo: figma.children('Product logo'),
+  },
+  example: (props) => (
+    <FocusedLayout background={props.background}>
+      <FocusedLayout.TopBar logo={props.logo} title={props.pageTitle}>
+        {props.closeButton}
+      </FocusedLayout.TopBar>
+      <FocusedLayout.Content>{/* TODO: add content */}</FocusedLayout.Content>
+      <FocusedLayout.BottomBar>{props.actions}</FocusedLayout.BottomBar>
+    </FocusedLayout>
+  ),
+})
+
+figma.connect(FocusedLayout, '<FOCUSED_LAYOUT_URL_DEPRECATED>', {
+  variant: { Breakpoints: 'MD-2XL' },
+  props: {
+    actions: figma.children('Button group'),
+    background: figma.enum('🎨 Style', {
+      Light: 'light',
+      Dark: 'dark',
+    }),
+    pageTitle: figma.string('✏️ Page title'),
+    logo: figma.children('Product logo'),
+  },
+  example: (props) => (
+    <FocusedLayout background={props.background}>
+      <FocusedLayout.TopBar logo={props.logo} title={props.pageTitle}>
+        {props.actions}
+      </FocusedLayout.TopBar>
       <FocusedLayout.Content>{/* TODO: add content */}</FocusedLayout.Content>
     </FocusedLayout>
   ),
