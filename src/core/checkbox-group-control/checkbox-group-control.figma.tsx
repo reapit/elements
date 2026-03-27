73116430a98bf065f5868d7a9b343a8f572d68eb
Kurt Doherty <kdoherty@reapit.com>
2026-03-27 10:20:02 +1000

    chore: DS-123 Connect new CheckboxGroupControl Figma component (#1238)
    ## Summary
    
    - Update `CheckboxGroupControl` Figma Code Connect to target the new
    Figma component node
    - Retain the deprecated Figma node as a fallback connection to preserve
    backwards compatibility during the Figma migration
    
    ## Ticket
    
    [DS-123](https://reapit.atlassian.net/browse/DS-123)
    
    [DS-123]:
    https://reapit-global.atlassian.net/browse/DS-123?atlOrigin=eyJpIjoiNWRkNTljNzYxNjVmNDY3MDlhMDU5Y2ZhYzA5YTRkZjUiLCJwIjoiZ2l0aHViLWNvbS1KU1cifQ

--- src/core/checkbox-group-control/checkbox-group-control.figma.tsx
+++ src/core/checkbox-group-control/checkbox-group-control.figma.tsx
@@ -3,6 +3,43 @@ import figma from '@figma/code-connect'
 
 figma.connect(CheckboxGroupControl, '<CHECKBOX_GROUP_URL>', {
   props: {
+    children: figma.enum('Orientation', {
+      Horizontal: figma.slot('Horizontal content slot'),
+      Vertical: figma.slot('Vertical content slot'),
+    }),
+    disabled: figma.enum('State', {
+      Disabled: true,
+    }),
+    errorText: figma.enum('State', {
+      Error: figma.string('Error message'),
+    }),
+    label: figma.boolean('Show group label', {
+      true: figma.nestedProps('LabelText', {
+        text: figma.string('Label text'),
+      }),
+      false: {
+        text: undefined,
+      },
+    }),
+    orientation: figma.enum('Orientation', {
+      Horizontal: 'horizontal',
+      Vertical: 'vertical',
+    }),
+  },
+  example: ({ children, disabled, errorText, label, orientation }) => (
+    <CheckboxGroupControl disabled={disabled} errorText={errorText} label={label.text} orientation={orientation}>
+      {/* NOTE: use CheckboxGroupControl.Option instead of CheckboxGroupControl */}
+      {children}
+    </CheckboxGroupControl>
+  ),
+})
+
+//
+// Deprecated Figma component support.
+//
+
+figma.connect(CheckboxGroupControl, '<CHECKBOX_GROUP_URL_DEPRECATED>', {
+  props: {
     children: figma.children('Checkbox *'),
     disabled: figma.enum('State', {
       Disabled: true,
