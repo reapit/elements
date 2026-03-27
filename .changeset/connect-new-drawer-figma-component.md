2c8cf98896a6bce5d8d72c80d67af254038ebfd2
Kurt Doherty <kdoherty@reapit.com>
2026-03-27 11:13:18 +1000

    chore: DS-125 Connect new Drawer Figma component (#1240)
    ## Summary
    
    - Connect `Drawer` to the new Figma component node, covering both Simple
    and With footer variants
    - Retain the old Figma node as a deprecated mapping to preserve existing
    Code Connect coverage
    
    ## Ticket
    
    [DS-125](https://reapit-global.atlassian.net/browse/DS-125)
    
    [DS-125]:
    https://reapit-global.atlassian.net/browse/DS-125?atlOrigin=eyJpIjoiNWRkNTljNzYxNjVmNDY3MDlhMDU5Y2ZhYzA5YTRkZjUiLCJwIjoiZ2l0aHViLWNvbS1KU1cifQ

---
+++ .changeset/connect-new-drawer-figma-component.md
@@ -0,0 +1,5 @@
+---
+'@reapit/elements': patch
+---
+
+Internal: Update `Drawer` Figma Code Connect to target the new Figma component node, retaining the deprecated node as a fallback connection
--- src/core/drawer/drawer.figma.tsx
+++ src/core/drawer/drawer.figma.tsx
@@ -4,6 +4,54 @@ import figma from '@figma/code-connect'
 figma.connect(Drawer, '<DRAWER_URL>', {
   variant: { Variant: 'Simple' },
   props: {
+    children: figma.slot('Content slot'),
+    overline: figma.string('Overline'),
+    supplementaryInfo: figma.children('Supplementary info'),
+    tabs: figma.children('Tabs'),
+    title: figma.string('Drawer title'),
+  },
+  example: (props) => (
+    <Drawer>
+      <Drawer.Header
+        action={<Drawer.HeaderCloseButton />}
+        overline={props.overline}
+        supplementaryInfo={props.supplementaryInfo}
+        tabs={props.tabs}
+      >
+        {props.title}
+      </Drawer.Header>
+      <Drawer.Body>{props.children}</Drawer.Body>
+    </Drawer>
+  ),
+})
+
+figma.connect(Drawer, '<DRAWER_URL>', {
+  variant: { Variant: 'With footer' },
+  props: {
+    children: figma.slot('Content slot'),
+    footer: figma.children('Button group'),
+    overline: figma.string('Overline'),
+    supplementaryInfo: figma.children('Supplementary info'),
+    title: figma.string('Drawer title'),
+  },
+  example: (props) => (
+    <Drawer>
+      <Drawer.Header overline={props.overline} supplementaryInfo={props.supplementaryInfo}>
+        {props.title}
+      </Drawer.Header>
+      <Drawer.Body>{props.children}</Drawer.Body>
+      <Drawer.Footer>{props.footer}</Drawer.Footer>
+    </Drawer>
+  ),
+})
+
+//
+// Deprecated Figma component support
+//
+
+figma.connect(Drawer, '<DRAWER_URL_DEPRECATED>', {
+  variant: { Variant: 'Simple' },
+  props: {
     overline: figma.string('Overline'),
     supplementaryInfo: figma.children('Supplementary info'),
     tabs: figma.children('Tabs'),
@@ -24,7 +72,7 @@ figma.connect(Drawer, '<DRAWER_URL>', {
   ),
 })
 
-figma.connect(Drawer, '<DRAWER_URL>', {
+figma.connect(Drawer, '<DRAWER_URL_DEPRECATED>', {
   variant: { Variant: 'With footer' },
   props: {
     footer: figma.children('Button group'),
