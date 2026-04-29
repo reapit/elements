093533861b71693146081c64e7b4800777aa181b
Kurt Doherty <kdoherty@reapit.com>
2026-04-29 12:04:57 +1000

    feat: DS-170 Add AlertBanner code connect (#1325)
    ## Summary
    
    - Add Figma Code Connect for `AlertBanner`, mapping variant, icon,
    dismiss, and action props to Figma properties
    - Register `<ALERT_BANNER_URL>` in `figma.config.json` and reorder
    Autocomplete entries alphabetically
    
    ## Ticket
    
    [DS-170](https://reapit-global.atlassian.net/browse/DS-170)
    
    
    [DS-170]:
    https://reapit-global.atlassian.net/browse/DS-170

---
+++ .changeset/ds-170-alert-banner-code-connect.md
@@ -0,0 +1,5 @@
+---
+'@reapit/elements': patch
+---
+
+Internal: Add Figma Code Connect for `AlertBanner`
---
+++ src/core/alert-banner/alert-banner.figma.tsx
@@ -0,0 +1,30 @@
+import { AlertBanner } from './alert-banner'
+import figma from '@figma/code-connect'
+
+figma.connect(AlertBanner, '<ALERT_BANNER_URL>', {
+  props: {
+    actions: figma.boolean('Show actions', {
+      true: figma.children('Button group'),
+      false: undefined,
+    }),
+    children: figma.string('Description'),
+    icon: figma.boolean('Show icon', {
+      true: figma.instance('Icon'),
+      false: undefined,
+    }),
+    onDismiss: figma.boolean('Dismissible', {
+      true: () => void 0,
+      false: undefined,
+    }),
+    variant: figma.enum('Variant', {
+      Warning: 'warning',
+      Info: 'info',
+      Error: 'error',
+    }),
+  },
+  example: (props) => (
+    <AlertBanner actions={props.actions} icon={props.icon} onDismiss={props.onDismiss} variant={props.variant}>
+      {props.children}
+    </AlertBanner>
+  ),
+})
